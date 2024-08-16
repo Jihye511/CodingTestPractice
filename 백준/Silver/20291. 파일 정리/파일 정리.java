3745a43183d5042a34866cc76126577cc422ebd8
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-08-16 14:52:11 +0900

    [Silver III] Title: 파일 정리, Time: 800 ms, Memory: 72988 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -4,31 +4,31 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(br.readLine());
-        Map<String, Integer> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
         int cnt;
-        for (int i =0; i<N; i++){
+        int N = Integer.parseInt(br.readLine());
+        Map<String,Integer> map = new HashMap<>();
+
+        for (int i = 0; i<N; i++){
             String input = br.readLine();
             String[] split = input.split("\\.");
+
             if(map.containsKey(split[1])){
-                cnt= map.get(split[1]);
+                cnt = map.get(split[1]);
                 map.put(split[1],++cnt);
-            }else{
+            }
+            else{
                 map.put(split[1],1);
             }
-
         }
+        //출력 정렬
         Object[] strings = map.keySet().toArray();
         Arrays.sort(strings);
 
+        StringBuilder sb = new StringBuilder();
         for(Object string : strings){
-            sb.append(string +" ");
+            sb.append(string +" " );
             sb.append(map.get(string));
             sb.append("\n");
         }

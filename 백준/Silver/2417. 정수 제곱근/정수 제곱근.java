cf03b8c5c6972213d5076ba485181bd283da53a5
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-09-21 18:55:36 +0900

    [Silver IV] Title: 정수 제곱근, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.java
@@ -1,27 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    static long n;
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        long n = Long.parseLong(br.readLine());
-        System.out.println(bSearch(n));
+        n = Long.parseLong(br.readLine());
+        System.out.println(Binary(n));
+        
     }
-
-    static long bSearch(long n){
-        long start = 0;
+    public static long Binary(long n){
+        long start =0;
         long end = n;
-        long result=0;
-
-        while(start<=end) {
-            long mid = (start + end) / 2;
-            if(n <= Math.pow(mid, 2)){
-                result=mid;
-                end=mid-1;
-            }else if(Math.pow(mid,2)<n){
-                start=mid+1;
+        long result = 0;
+        while(start <= end){
+            long mid = (start + end)/2;
+            if(n <= Math.pow(mid,2)){
+                result = mid;
+                end = mid-1;
+            }else {
+                start = mid+1;
             }
         }
         return result;

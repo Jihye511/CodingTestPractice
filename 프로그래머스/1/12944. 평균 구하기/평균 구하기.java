9960339bc72244c7e2399a9f6e4851777f2138c5
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-01 23:51:04 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double hap=0;        
+        for(int i=0; i<arr.length; i++){
+            hap +=arr[i];
+        }
+        answer = (double)(hap/arr.length);
+        return answer;
+    }
+}

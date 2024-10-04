35ef5e01834fa7560d31404ae1810717a4c05e5d
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-04 21:21:50 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1008 ms, Memory: 67644 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,52 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-    static int n;
-    static int[] parents;
-    static ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
-    static boolean[] visit;
-
+    static int N;
+    static ArrayList<Integer>[] node;
+    static int[] ans;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
 
-        for(int i =0; i<=n; i++){
-            edges.add(new ArrayList<>());
+        N = Integer.parseInt(br.readLine());
+        node = new ArrayList[N+1];
+        ans = new int[N+1];
+        visited = new boolean[N+1];
+        for(int i =0; i<N+1; i++){
+            node[i] = new ArrayList<>();
         }
-
-        for(int i = 0; i<n-1; i++){
+        for(int i=0; i<N-1;i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            edges.get(a).add(b);
-            edges.get(b).add(a);
+            node[a].add(b);
+            node[b].add(a);
         }
-        parents = new int[n+1];
-        visit = new boolean[n+1];
-
         bfs(1);
-        for(int i =2; i<=n; i++){
-            System.out.println(parents[i]);
+        for(int i =2; i<=N; i++){
+            System.out.println(ans[i]);
         }
     }
-    public static void bfs(int node){
-        visit[node] = true;
-        //인접 노드 조회
-        for(int n : edges.get(node)){
-            if(!visit[n]){
-                //인접 노드의 부모를 현재 노드로 설정
-                parents[n] = node;
-                //인접 노드를 현재 노드로
-                bfs(n);
+    public static void bfs(int n){
+        visited[n]= true;
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(n);
+        while(!q.isEmpty()){
+            int current = q.poll();
+            for(int i =0; i<node[current].size(); i++){
+                int next = node[current].get(i);
+                if(!visited[next]){
+                    visited[next] = true;
+                    ans[next] = current;
+                    q.offer(next);
+                }
             }
+
         }
     }
 

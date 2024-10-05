8b361202183554415b6ba42540bdd2c29d0ed73f
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-06 01:23:47 +0900

    [Silver III] Title: 파일 정리, Time: 776 ms, Memory: 66372 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,37 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    static int N;
+    static HashSet<String> hash = new HashSet<>();
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int cnt;
-        int N = Integer.parseInt(br.readLine());
         Map<String,Integer> map = new HashMap<>();
-
-        for (int i = 0; i<N; i++){
+        N = Integer.parseInt(br.readLine());
+        for(int i =0; i<N; i++){
             String input = br.readLine();
             String[] split = input.split("\\.");
-
             if(map.containsKey(split[1])){
                 cnt = map.get(split[1]);
                 map.put(split[1],++cnt);
-            }
-            else{
+            }else{
                 map.put(split[1],1);
             }
         }
         //출력 정렬
         Object[] strings = map.keySet().toArray();
-        Arrays.sort(strings);
 
+        Arrays.sort(strings);
         StringBuilder sb = new StringBuilder();
-        for(Object string : strings){
-            sb.append(string +" " );
-            sb.append(map.get(string));
+        for(Object a : strings){
+            sb.append(a).append(" ").append(map.get(a));
             sb.append("\n");
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
+
     }
 }

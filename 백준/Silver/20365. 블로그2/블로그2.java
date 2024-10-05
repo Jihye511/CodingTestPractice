a035358e1b7a4ee9ef50092d8ecff2760791b64f
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-06 00:56:04 +0900

    [Silver III] Title: 블로그2, Time: 200 ms, Memory: 19888 KB -BaekjoonHub

--- 백준/Silver/20365. 블로그2/블로그2.java
+++ 백준/Silver/20365. 블로그2/블로그2.java
@@ -1,29 +1,30 @@
-import java.util.*;
 import java.io.*;
-
-public class Main{
+import java.util.*;
+public class Main {
     static int n;
-    static char[] work;
-    static int blue, red, cnt;
-    public static void main(String[] args) throws IOException{
+    static char[] s;
+    static int blue=0;
+    static int red=0;
+    static int result=0;
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        work = new char[n];
-//        StringTokenizer st = new StringTokenizer(br.readLine());
+        s = new char[n];
         String str = br.readLine();
         for(int i =0; i<n; i++){
-            work[i] = str.charAt(i);
+            s[i] = str.charAt(i);
         }
-        char before ='X';
+        char before='X';
         for(int i =0; i<n; i++){
-            if(work[i] !=before){
-                if(work[i] =='R') red++;
-                else if(work[i] =='B') blue++;
-                before =work[i];
+            if(s[i] !=before){
+                if(s[i]=='R') red++;
+                else if(s[i]=='B')blue++;
+                before = s[i];
             }
         }
-        cnt = Math.min(red,blue) +1;
-        System.out.println(cnt);
+        result = Math.min(red,blue) +1;
+        System.out.println(result);
+
     }
 }

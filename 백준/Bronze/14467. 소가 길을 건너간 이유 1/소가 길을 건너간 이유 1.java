b25f6c9452c7657d41abb2a25b534721689fce50
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-06 09:12:02 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.java
@@ -1,39 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.StringTokenizer;
-
-// 이문제는.. 해당 숫자가 2번이상 나와서 0,1이 바뀐다면 count 증가되게 해서 .. 해야할 듯
+import java.io.*;
+import java.util.*;
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(st.nextToken());
-        int count=0;
-        int[] arr = new int[]{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-
-        //실험 for문
-        for(int i = 0; i<N; i++){
-            st = new StringTokenizer(bf.readLine());
+    static int N,cnt;
+    static int[] cowList;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        cowList = new int[11];
+        Arrays.fill(cowList,-1);
+        for(int i =0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
             int cow = Integer.parseInt(st.nextToken());
-            int location = Integer.parseInt(st.nextToken());
+            int locate = Integer.parseInt(st.nextToken());
 
-            if(arr[cow]==-1){
-                arr[cow]=location;
-            }else if(arr[cow]!= location){
-                arr[cow]^=1; //^= 비트연산자 XOR : 두비트가 다를 때 1, 같을 때
-                count ++;
-            }
+            if(cowList[cow]==-1){
+                cowList[cow] =locate;
+            }else if(cowList[cow] !=locate){
+                cowList[cow] =locate;
+                cnt++;
+            }else continue;
+
         }
-        System.out.println(count);
-
-
-
-
+        System.out.println(cnt);
     }
-
-
 }

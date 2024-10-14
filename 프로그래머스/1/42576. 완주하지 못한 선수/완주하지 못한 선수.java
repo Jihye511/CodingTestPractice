3e51cf3e4b2da1e454497c2cca606fd6d7c77faf
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-15 08:37:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 308.69 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,21 @@
 import java.io.*;
 import java.util.*;
-
 class Solution {
+    
+    
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
         Arrays.sort(participant);
         Arrays.sort(completion);
-        int N= completion.length;
-        for(int i =0; i<N; i++){
-            if(participant[i].equals(completion[i])){
-                continue;
-            }
-            else{
-                return participant[i];
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                return answer;
             }
         }
-        return participant[N];
+        
+        // 마지막에 남는 선수는 participant 배열의 마지막 요소
+        return participant[participant.length - 1];
     }
 }

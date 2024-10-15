96113806dd05ae326a8538fdc12c2bde697d430f
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-15 19:14:36 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 668 ms, Memory: 53072 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,82 +1,81 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
     static int n,m;
     static int[][] map;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+    static int[][] result;
     static boolean[][] visited;
-    static int i_x,i_y;
-    public static void bfs(int i, int j){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{i,j});
-        visited[i][j] = true;
-        map[i][j]=0;
-        while(!q.isEmpty()){
-            int[] point = q.poll();
-            int x = point[0];
-            int y = point[1];
-
-            for(int k =0; k<4; k++){
-                int nx = x +dx[k];
-                int ny = y +dy[k];
-
-                //미로 벗어난 경우 무시
-                if(nx<0 || ny<0 || nx>=n || ny >=m) continue;
-                //0인 경우 무시
-                if (map[nx][ny] == 0) continue;
-                //이미 방문한 경우 무시
-                if(visited[nx][ny]) continue;
-                //1인 경우 (처음 반복) 거리 기록
-                q.offer(new int[]{nx, ny});
-                map[nx][ny] = map[x][y] + 1;
-                visited[nx][ny] = true;
-            }
-        }
-
-    }
-    public static void main(String[] args) throws IOException{
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0 ,0, -1, 1};
+    static int x=0,y=0;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n= Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map=new int[n][m];
+
+        map = new int[n][m];
+        result = new int[n][m];
         visited = new boolean[n][m];
-        //map에 입력값 집어넣기
+
+        for(int i =0; i<n; i++){
+            Arrays.fill(result[i],-1);
+        }
         for(int i =0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<m; j++){
+            for(int j =0; j<m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        //2인 인덱스 찾아서 bfs 호출
-        for(int i =0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(map[i][j] ==2){
-                    i_x=i;
-                    i_y=j;
-                }else if(map[i][j] ==0){
-                    visited[i][j]=true;
+                if(map[i][j]==2){
+                    x =i;
+                    y =j;
+                    visited[i][j] =true;
+                    result[i][j] =0;
+                }
+                if(map[i][j]==0){
+                    result[i][j]=0;
                 }
             }
         }
-        bfs(i_x,i_y);
-        
-        
-        //출력
+        bfs(x,y);
         for(int i =0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(!visited[i][j]){
-                    map[i][j]=-1;
+            for(int j =0; j<m; j++){
+                if(map[i][j] ==1 && result[i][j] ==-1){
+                    result[i][j]=-1;
                 }
+                sb.append(result[i][j]).append(" ");
+
             }
+            sb.append("\n");
         }
-        for(int i =0; i<n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(map[i][j]+" ");
+        System.out.println(sb);
+    }
+    public static void bfs(int i, int j){
+        Queue<int []> q = new LinkedList<>();
+        q.offer(new int[] {i,j});
+
+        while(!q.isEmpty()){
+            int[] current = q.poll();
+            int cx = current[0];
+            int cy = current[1];
+            for(int d=0;d<4;d++){
+                int nx = cx +dx[d];
+                int ny = cy +dy[d];
+
+                if(nx>=0 && nx <n && ny>=0 &&ny<m){
+                    if(!visited[nx][ny] && map[nx][ny]==1){
+                        visited[nx][ny] =true;
+                        result[nx][ny]= result[cx][cy]+1;
+                        q.offer(new int[] {nx,ny});
+                    }
+                }
+
             }
-            System.out.println();
+
+
+
         }
     }
 }

263e64456c621cb3f86538deb2e3a698579431b4
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-16 15:42:26 +0900

    [Silver I] Title: 경로 찾기, Time: 148 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,52 +1,36 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int N;
     static int[][] map;
-    static boolean[] visited;
+    static int N;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args)throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
 
-        N= Integer.parseInt(br.readLine());
         map = new int[N][N];
-        for(int i =0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j =0;j <N; j++){
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
                 map[i][j] =Integer.parseInt(st.nextToken());
             }
         }
         for(int i =0; i<N; i++){
-            bfs(i);
+            for(int j=0; j<N; j++){
+                for(int k=0; k<N; k++){
+                    if(map[j][i]==1 && map[i][k]==1){
+                        map[j][k]=1;
+                    }
+                }
+            }
         }
-
-        for(int i =0; i<N; i++){
-            for(int j = 0; j<N; j++){
+        for(int i =0;i<N; i++){
+            for(int j =0; j<N; j++){
                 sb.append(map[i][j]).append(" ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
-    }
-    public static void bfs(int node){
-        visited = new boolean[N];
-        Queue<Integer> q = new LinkedList<>();
-        for(int i =0; i<N;i++){
-            if(map[node][i]==1) q.offer(i);
-        }
-        while(!q.isEmpty()){
-            int cur = q.poll();
-
-            for(int next=0; next<N; next++){
-                if(map[cur][next]==1){
-                    if(!visited[next]) {
-                        visited[next] = true;
-                        map[node][next] = 1;
-                        q.offer(next);
-                    }
-
-                }
-            }
-        }
     }
 }

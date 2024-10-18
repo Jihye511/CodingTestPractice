6f29a9960155343c0960100fbeb89c3cd3853a66
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-18 20:23:08 +0900

    [Silver II] Title: 나무 자르기, Time: 928 ms, Memory: 119464 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,44 +1,52 @@
-import java.util.*;
 import java.io.*;
-
-public class Main{
+import java.nio.Buffer;
+import java.util.*;
+public class Main {
     static int N;
-    static long M;
-    static int[] tree;
-    public static void main(String[] args)throws IOException{
+    static Long M;
+    static int[] trees;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Long.parseLong(st.nextToken());
 
-        tree = new int[N];
+        trees = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i =0; i<N; i++){
-            tree[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(tree);
-        System.out.println(bSearch());
+        Arrays.sort(trees);
+
+        System.out.println(binarySearch(M));
     }
-    public static int bSearch(){
-        int left =0;
-        int right = tree[N-1];
-        int result=0;
-        while(left<=right){
-            int mid = (left+right)/2;
-            long extra =0;
-            for(int i =0; i<N; i++){
-                if(tree[i]-mid>0){
-                    extra += tree[i]-mid;
-                }
-            }
-            if(extra >=M){
+    public static long binarySearch(long m ){
+        long start = 0;
+        long end = trees[N-1];
+        long result =0;
+        while(start<= end){
+            long mid= (start + end)/2;
+            long extra = calculate(mid);
+            if(extra >=m){
                 result = mid;
-                left = mid+1;
-            }else {
-                right = mid -1;
+                start = mid+1;
+            }else{
+                end = mid -1;
             }
         }
         return result;
     }
+    public static long calculate(long m){
+        long sum=0;
+        for(int i =0; i<N; i++){
+            if(trees[i]>m){
+                sum += trees[i]-m;
+            }
+        }
+        return sum;
+    }
+
+
 }

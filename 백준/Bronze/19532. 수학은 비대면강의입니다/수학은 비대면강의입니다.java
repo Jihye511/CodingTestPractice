7876974b2164d724e80f72971d8fa7574d1a7267
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-21 15:27:46 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 144 ms, Memory: 16096 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,32 +1,25 @@
-import java.util.*;
 import java.io.*;
-
+import java.util.*;
 public class Main{
-    static int a,b,c,d,e,f;
+    static int [] var = new int[6];
     static int x,y;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args )throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i =0; i<6;i++){
+            var[i] = Integer.parseInt(st.nextToken());
+        }
 
-        a =Integer.parseInt(str[0]);
-        b =Integer.parseInt(str[1]);
-        c =Integer.parseInt(str[2]);
-        d =Integer.parseInt(str[3]);
-        e =Integer.parseInt(str[4]);
-        f =Integer.parseInt(str[5]);
-        x =0;
-        y =0;
         for(int i =-999; i<=999; i++){
-            for(int j =-999; j<=999;j++ ){
-                if((a*i +b*j == c) && (d*i + e*j ==f)){
-                    x = i;
-                    y = j;
-                    break;
+            for(int j =-999; j<=999; j++){
+                if(var[0]*i + var[1]*j == var[2]) {
+                    if(var[3] *i +var[4]*j == var[5]){
+                        x= i;
+                        y =j;
+                    }
                 }
             }
         }
-        System.out.println(x +" " +y);
+        System.out.println(x + " "+y);
     }
-
 }

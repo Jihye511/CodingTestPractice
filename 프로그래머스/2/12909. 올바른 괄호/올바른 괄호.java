6eafdcf77ee70bc4014481dd89c5cbdb5d9596f9
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-23 19:27:03 +0900

    [level 2] Title: 올바른 괄호, Time: 5.66 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,22 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        int count=0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) =='('){
-                count ++;
-            }else if(s.charAt(i) == ')'){
-                count --;
-            }
-            if(count<0) return false;
-            
-        }
-        if(count==0) answer = true;
-        else{
-            answer =false;
-        }
-            
+        // 배열로 쪼개기
+        int cnt=0;
+        char[] c = s.toCharArray();
+        if(c[0] =='(' && c[c.length-1] ==')'){
+            for(int i =0; i<c.length; i++){
+                if(c[i] =='(') cnt ++;
+                else if(c[i]==')') cnt --;
+                
+                if(cnt<0) answer =false;
+            } 
+            if(cnt != 0) answer = false;
+        }else answer = false;
+        
         return answer;
     }
 }

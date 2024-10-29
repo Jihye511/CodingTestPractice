7fcaced40ae2fda97b4b77bd420b291453c85f92
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-30 08:16:33 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,14 +1,21 @@
 class Solution {
     public int solution(long num) {
-        int answer = 0;
-        while(num !=1){
-            if(answer ==500) return -1;
-            if(num%2 == 0){
-                num /=2;
-            }else {
-                num = num*3 +1;
+        int answer = -1;
+        int cnt=0;
+        for(int i =0; i<500;i++){
+            if(num ==1){
+                answer = cnt;
+                break;
             }
-            answer ++;
+            if(num %2==0){
+                num /=2;
+                cnt ++;
+            }
+            else{
+                num = num*3 +1;
+                cnt++;
+            }
+            
         }
         return answer;
     }

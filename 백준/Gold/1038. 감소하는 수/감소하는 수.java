2dd3eb30803c7978cbedbfbaf902c379a8ff5238
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-10-31 11:53:50 +0900

    [Gold V] Title: 감소하는 수, Time: 136 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static int N;
+    static int[] arr ={9,8,7,6,5,4,3,2,1,0};
+    static ArrayList<Long> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dfs(0,0);
+        Collections.sort(list);
+        if(list.size()<N+1){
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(list.get(N));
+    }
+
+    public static void dfs(long num, int idx){
+
+
+        if(idx==10){
+            if(!list.contains(num)){
+                list.add(num);
+            }
+            return;
+        }
+        dfs(num*10 + arr[idx], idx+1);
+        dfs(num, idx+1);
+    }
+}

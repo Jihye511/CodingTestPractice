0644cd794859fd6c551e6fac5b28f216c5c38358
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-11-01 08:16:52 +0900

    [level 1] Title: K번째수, Time: 0.30 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,22 +2,15 @@ import java.io.*;
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        int k = commands.length;
-        answer = new int[k];
-        
-        for(int i =0; i<k; i++){
+        int[] answer = new int[commands.length];
+        for(int i =0; i<commands.length; i++){
+            int[] cur = commands[i];
             ArrayList<Integer> list = new ArrayList<>();
-            int start = commands[i][0]-1;
-            int end = commands[i][1]-1;
-            int v = commands[i][2]-1;
-            for(int j=0; j<array.length; j++){
-                if(start <= j && end>=j){
-                    list.add(array[j]);
-                }
+            for(int j =cur[0]-1; j<cur[1]; j++){
+                list.add(array[j]);
             }
             Collections.sort(list);
-            answer[i]=list.get(v);
+            answer[i] = list.get(cur[2]-1);
         }
         return answer;
     }

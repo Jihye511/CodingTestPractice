7fad42c231a0472069160cc57b4fb46e22c11e57
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-11-07 08:26:47 +0900

    [level 1] Title: 모의고사, Time: 2.06 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,29 +2,34 @@ import java.io.*;
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-       
-        int [] p1 = {1,2,3,4,5};
-        int [] p2 = {2,1,2,3,2,4,2,5};
-        int [] p3 = {3,3,1,1,2,2,4,4,5,5};
-        int[] sum = new int [3];
-        for(int i=0; i<answers.length; i++){
-            if(answers[i] == p1[i%p1.length]) sum[0]++;
-            if(answers[i] == p2[i%p2.length]) sum[1]++;
-            if(answers[i] == p3[i%p3.length]) sum[2]++;
+        
+        int[][] student = {{1,2,3,4,5},
+                           {2,1,2,3,2,4,2,5},
+                           {3,3,1,1,2,2,4,4,5,5}
+                           };
+        int[] ans = new int[3];
+        for(int i= 0; i<answers.length; i++){
+            for(int j =0; j<3; j++){
+                if(answers[i] == student[j][i%student[j].length]){
+                    ans[j]++;
+                }
+            }
         }
         ArrayList<Integer> list = new ArrayList<>();
-        int max = Math.max(sum[0],Math.max(sum[1],sum[2]));
-        for(int i =0; i<3; i++){
-            if(max == sum[i]){
+        int max =-1;
+        for(int i: ans){
+            max= Math.max(i,max);
+        }
+        for(int i=0; i<3; i++){
+            if(max == ans[i]){
                 list.add(i+1);
             }
         }
         Collections.sort(list);
-         int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
+        int[] answer = new int[list.size()];
+        for(int n=0; n<list.size(); n++){
+            answer[n] = list.get(n);
         }
-        
         return answer;
     }
 }

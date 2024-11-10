6477aed932d5d5a3a08bc9b7e4d5826abc786963
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-11-10 23:56:45 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,52 +1,64 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
     static int n;
     static int[][] map;
-    static int count;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
-        map= new int[n][n];
-        String str;
+        map = new int[n][n];
         for(int i =0; i<n; i++){
-            str = br.readLine();
+            String input = br.readLine();
             for(int j =0; j<n; j++){
-                map[i][j] = str.charAt(j)-'0';
+                map[i][j] = input.charAt(j) - '0';
             }
+
         }
-        ArrayList<Integer> arr = new ArrayList<>();
-        int result = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                count = 0;
-                if(map[i][j] == 1){
+        int count=0;
+        for(int i =0; i<n; i++){
+            for(int j =0; j<n; j++){
+                if(map[i][j] !=0){
                     dfs(i,j);
-                    arr.add(count);
-                    result++;
+                    count++;
                 }
             }
         }
-
-        Collections.sort(arr);
-        System.out.println(result);
-        for(int i : arr){
-            System.out.println(i);
+        Collections.sort(list);
+        sb.append(count).append("\n");
+        for(int n : list){
+            sb.append(n).append("\n");
         }
+        System.out.println(sb);
+
     }
     public static void dfs(int x, int y){
-        map[x][y] =0;
-        count += 1;
+        int cnt=1;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x,y});
+        map[x][y]=0;
+        while(!q.isEmpty()){
+            int[] current = q.poll();
+            for(int i =0; i<4; i++){
+                int nx = current[0] + dx[i];
+                int ny = current[1] + dy[i];
 
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx>=0 && nx<n && ny>=0 && ny<n && map[nx][ny]==1) dfs(nx, ny);
+                //범위 밖이면 패쓰
+                if(nx<0 || nx>=n || ny<0 || ny>=n) continue;
+                //0이면 패쓰
+                if(map[nx][ny] !=0){
+                    map[nx][ny] =0;
+                    cnt++;
+                    q.offer(new int[]{nx,ny});
+                }
+            }
         }
+        list.add(cnt);
+//        return cnt;
     }
 }

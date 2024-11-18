f2822c69b6d5cc0c6fa8a88f47740754fb4910a4
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-11-18 20:16:55 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 632 ms, Memory: 52852 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,81 +1,75 @@
 import java.io.*;
+import java.sql.PreparedStatement;
 import java.util.*;
 
 public class Main {
-    static int n,m;
+    static int n, m;
     static int[][] map;
-    static int[][] result;
+    static int[][] dis;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     static boolean[][] visited;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0 ,0, -1, 1};
-    static int x=0,y=0;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args)throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
-        result = new int[n][m];
+        dis = new int[n][m];
         visited = new boolean[n][m];
-
-        for(int i =0; i<n; i++){
-            Arrays.fill(result[i],-1);
-        }
-        for(int i =0; i<n; i++){
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j =0; j<m; j++){
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j]==2){
-                    x =i;
-                    y =j;
+                if (map[i][j] == 2) {
+                    map[i][j] = 0;
                     visited[i][j] =true;
-                    result[i][j] =0;
-                }
-                if(map[i][j]==0){
-                    result[i][j]=0;
+                    x = i;
+                    y = j;
                 }
             }
         }
-        bfs(x,y);
-        for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                if(map[i][j] ==1 && result[i][j] ==-1){
-                    result[i][j]=-1;
+        bfs(x, y);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(!visited[i][j] &&map[i][j]==1){
+                    sb.append(-1).append(" ");
+                }else{
+                    sb.append(map[i][j]).append(" ");
                 }
-                sb.append(result[i][j]).append(" ");
 
             }
             sb.append("\n");
         }
         System.out.println(sb);
     }
-    public static void bfs(int i, int j){
-        Queue<int []> q = new LinkedList<>();
-        q.offer(new int[] {i,j});
 
-        while(!q.isEmpty()){
-            int[] current = q.poll();
-            int cx = current[0];
-            int cy = current[1];
-            for(int d=0;d<4;d++){
-                int nx = cx +dx[d];
-                int ny = cy +dy[d];
+    public static void bfs(int x, int y) {
+        map[x][y] =0;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int []{x,y});
+        while (!q.isEmpty()){
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            for(int i =0; i<4; i++){
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
-                if(nx>=0 && nx <n && ny>=0 &&ny<m){
-                    if(!visited[nx][ny] && map[nx][ny]==1){
-                        visited[nx][ny] =true;
-                        result[nx][ny]= result[cx][cy]+1;
-                        q.offer(new int[] {nx,ny});
-                    }
+                if(nx< 0 || nx>=n || ny<0 || ny>=m) continue;
+                if(map[nx][ny]==0) continue;
+
+                if(!visited[nx][ny]){
+                    visited[nx][ny] =true;
+                    map[nx][ny] = map[cx][cy]+1;
+                    q.offer(new int[]{nx,ny});
                 }
-
             }
-
-
-
         }
     }
 }

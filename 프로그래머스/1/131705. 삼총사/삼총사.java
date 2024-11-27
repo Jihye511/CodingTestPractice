0811c4edb21d8124195578ff8f280ac94eb5b25f
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-11-27 13:46:35 +0900

    [level 1] Title: 삼총사, Time: 0.55 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,17 +1,22 @@
-import java.io.*;
-import java.util.*;
 class Solution {
+    static int length;
+    static int cnt=0;
+    static int[] temp;
     public int solution(int[] number) {
-        int cnt=0;
-        for(int i=0; i<number.length-2; i++){
-            for(int j =i+1; j<number.length-1;j++){
-                for(int k =j+1; k<number.length; k++){
-                    if(number[i] + number[j] + number[k] == 0){
-                        cnt +=1;
-                    }
-                }
-            }
-        }
+        int answer = 0;
+        temp = number;
+        length = number.length;
+        dfs(0,0,0);
         return cnt;
+    }
+    public static void dfs(int depth, int hap, int size){
+        if(depth ==length){
+            if(size==3  && hap==0){
+                cnt+=1;
+            }
+            return;
+        }
+        dfs(depth+1, hap + temp[depth],size+1);
+        dfs(depth+1, hap, size);
     }
 }

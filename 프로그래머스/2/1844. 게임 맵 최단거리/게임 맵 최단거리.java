89e6a06dd37d4e1c6c70a65501d67700c862f283
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-12-04 13:48:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.18 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,39 @@
 import java.io.*;
 import java.util.*;
 class Solution {
-    static int[][] result;
-    static int n,m;
-    static int[] dx = {-1,1,0,0};
+    static int[] dx ={-1,1,0,0};
     static int[] dy = {0,0,-1,1};
     public int solution(int[][] maps) {
         int answer = 0;
-        n  = maps.length;
-        m = maps[0].length;
-
-        dfs(maps);
-        if(maps[n-1][m-1] ==1){
-            answer =-1;
-        }else{
-            answer = maps[n-1][m-1];
+        int m = maps.length;
+        int n = maps[0].length;
+        bfs(maps);
+        if(maps[m-1][n-1] ==1){
+            return -1;
         }
-        
-        return answer;
+        return maps[m-1][n-1];
     }
-    public static void dfs(int[][] maps){
-        Queue<int[]> q= new ArrayDeque<>();
-        q.offer(new int[]{0,0});
+    public static void bfs(int[][]maps){
+    
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {0,0});
         while(!q.isEmpty()){
-            int[] now = q.poll();
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            
             for(int i =0; i<4; i++){
-                int nx = now[0] +dx[i];
-                int ny = now[1] +dy[i];
-                if(isRange(nx,ny) &&maps[nx][ny] ==1){
-                    maps[nx][ny] +=maps[now[0]][now[1]];
-                    q.offer(new int[] {nx,ny});
-                }
+                int nx = cx+dx[i];
+                int ny = cy +dy[i];
+                
+                if(nx<0|| nx>=maps.length ||ny<0 || ny>=maps[0].length) continue;
+                
+                if(maps[nx][ny] !=1) continue;
+                
+                maps[nx][ny] +=maps[cx][cy];
+                q.offer(new int[]{nx,ny});
+                
             }
-          
         }
-    }
-    public static boolean isRange(int x, int y){
-        return (x>=0 && x<n && y>=0 && y<m);
     }
 }

5db47192c72f2577cf519927009b272cef7a719f
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-12-05 21:53:42 +0900

    [level 3] Title: 단어 변환, Time: 0.51 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,60 +1,34 @@
-import java.io.*;
-import java.util.*;
 class Solution {
-    static ArrayList<Integer> cntList = new ArrayList<>();
-    static boolean[] visited;
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        visited = new boolean[words.length];
-        if(!isThere(target,words)){
-            return 0;
-        }
-        
-        dfs(begin,target,words,0,0);
-        
-        Collections.sort(cntList);
-        answer = cntList.get(0);
-        
-        return answer;
+        boolean[] visited = new boolean[words.length];
+        int answer = dfs(target, begin, words, visited, 0);
+        return answer == Integer.MAX_VALUE ? 0 : answer;
     }
-    public static void dfs(String begin, String target, String[] words, int idx, int cnt){
-        if(begin.equals(target)){
-            cntList.add(cnt);
-            return;
+    public static int dfs(String target, String current, String[] words, boolean[] visited, int count) {
+         if (current.equals(target)) {
+            return count; // 변환 횟수 반환
         }
-        if(idx >= words.length) return;
-        for(int i =0; i<words.length; i++){
-            if(!visited[i]){
-                if(checkChange(begin, words[i])){ //한개 바꿀수 있으면 그걸로 저장
-                    String nextWord = words[i];
-                    visited[i] =true;
-                    dfs(nextWord, target,words,i+1, cnt+1);
-                    visited[i] =false;
-                }
-            }
-            
-        }
-    }
-    public static boolean checkChange(String begin, String word){
-        char [] begin_c = begin.toCharArray();
-        char[] word_c = word.toCharArray();
-        int count=0;
-        for(int i =0; i<begin.length(); i++){
-            if(begin_c[i]!=word_c[i]){
-                count ++;
+        int minCount = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (!visited[i] && checkWords(current, words[i])) {
+                visited[i] = true;
+                minCount = Math.min(minCount, dfs(target, words[i], words, visited, count + 1));
+                visited[i] = false; 
             }
         }
-        if(count ==1) return true;
-        else{
-            return false;
-        }
+        
+        return minCount;
     }
-    public static boolean isThere(String target , String[]words){
-        for(int i=0; i<words.length; i++){
-            if(target.equals(words[i])){
-                return true;
+    public static boolean checkWords(String str, String temp){
+        int cnt=0;
+        for(int i =0; i<str.length(); i++){
+            char c = str.charAt(i);
+            char n = temp.charAt(i);
+            if(c !=n ){
+                cnt++;
             }
         }
-        return false;
+        if(cnt ==1) return true;
+        else return false;
     }
 }

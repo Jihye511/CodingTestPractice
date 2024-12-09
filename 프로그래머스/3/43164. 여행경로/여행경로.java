7513d095ce9010752dd6e21f50618a401b6878e3
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2024-12-09 20:51:23 +0900

    [level 3] Title: 여행경로, Time: 80.19 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,29 +1,28 @@
 import java.io.*;
 import java.util.*;
 class Solution {
-    static ArrayList<String> list = new ArrayList<>();
+    static ArrayList<String> list;
     static boolean[] visited;
-    
     public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        list = new ArrayList<>();
         visited = new boolean[tickets.length];
-        dfs("ICN",0,"ICN",tickets);
+        dfs("ICN","ICN", 0,tickets);
         Collections.sort(list);
-        // String[] answer = new String[list.size()];
-        // for (int i = 0; i < list.size(); i++) {
-        //     answer[i] = list.get(i);
-        // }
-        return list.get(0).split(" ");
-    
+        answer = list.get(0).split(" ");
+        return answer;
     }
-    public static void dfs(String now, int depth,String path, String[][] tickets){
-        if(depth == tickets.length){
+    public static void dfs(String start,String path, int idx, String[][] tickets){
+        if(idx == tickets.length){
             list.add(path);
+            return;
         }
-        for(int i =0;i<tickets.length; i++){
-            if(!visited[i] && now.equals(tickets[i][0])){
-                visited[i]=true;
-                dfs(tickets[i][1],depth+1, path +" "+ tickets[i][1], tickets);
-                visited[i] =false;
+        
+        for(int i =0; i<tickets.length; i++){
+            if(start.equals(tickets[i][0])&&!visited[i]){
+                visited[i] = true;
+                dfs(tickets[i][1],path+" "+tickets[i][1], idx+1, tickets);
+                visited[i] = false;
             }
         }
     }

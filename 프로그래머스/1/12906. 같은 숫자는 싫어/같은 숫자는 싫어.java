99f6276457db68a5b2928e38d4a65b6f52af3ba1
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-01-10 22:49:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.66 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,20 +2,23 @@ import java.util.*;
 import java.io.*;
 public class Solution {
     public int[] solution(int []arr) {
-        
-        int before= arr[0];
+        int before = arr[0];
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
-        for(int i =1; i<arr.length; i++){
-            if(before != arr[i]){
-                before= arr[i];
-                list.add(arr[i]);
+        list.add(before);
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] ==before){
+                continue;
+            }
+            else{
+                before = arr[i];
+                list.add(before);
             }
         }
-        int[] answer = new int[list.size()];
+        int[] answer =new int[list.size()];
         int i =0;
-        for(int n :list){
-            answer[i++] = n;
+        for(int v : list){
+            answer[i] = v;
+            i++;
         }
         return answer;
     }

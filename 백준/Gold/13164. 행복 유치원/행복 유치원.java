79a61cc2414031c84247b50987a7d01949f298be
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-01-23 23:03:42 +0900

    [Gold V] Title: 행복 유치원, Time: 600 ms, Memory: 52672 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,38 +1,34 @@
-import java.util.*;
+
+
 import java.io.*;
-
+import java.util.*;
 public class Main {
-    static int N, K;
-    static int[] students;
-
-    public static void main(String[] args) throws IOException {
+    static int N,K;
+    static int[] child;
+    static int[] subTall;
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        students = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            students[i] = Integer.parseInt(st.nextToken());
+        child = new int[N];
+        subTall = new int[N-1];
+        st= new StringTokenizer(br.readLine());
+        for(int i =0; i<N; i++){
+            child[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 키 차이 계산
-        int[] key = new int[N - 1];
-        for (int i = 1; i < N; i++) {
-            key[i - 1] = students[i] - students[i - 1];
+        //키 차이 저장
+        for(int i =0; i<N-1; i++){
+            subTall[i] = child[i+1]-child[i];
         }
-
-        // 키 차이 정렬
-        Arrays.sort(key);
-
-        // 최소 비용 계산: 가장 큰 K-1개의 차이를 제외한 나머지 합산
-        int sum = 0;
-        for (int i = 0; i < N - K; i++) {
-            sum += key[i];
+        Arrays.sort(subTall);
+        int cnt=0;
+        for(int i =0; i<N-K; i++){
+            cnt+= subTall[i];
         }
-
-        System.out.println(sum);
+        System.out.println(cnt);
     }
 }

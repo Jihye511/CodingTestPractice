eb90ccd7eb8080f5a6a87fca0d17c6d09ed38b8b
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-01-30 16:36:24 +0900

    [D4] Title: 쇠막대기 자르기, Time: 129 ms, Memory: 37,728 KB -BaekjoonHub

---
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t =1; t<=T ; t++){
+            int cnt=0; //잘린 조각 개수
+            int n = 0; //현재 막대 개수
+            String str = br.readLine();
+
+            for(int i =0; i<str.length(); i++){
+                char c = str.charAt(i);
+
+                if(c =='('){
+                    n++;
+                }else{
+                    n--;
+                    if(str.charAt(i-1)=='('){
+                        cnt +=n;
+                    }else{
+                        cnt+=1;
+                    }
+                }
+            }
+            System.out.println("#" + t + " " + cnt);
+        }
+    }
+}

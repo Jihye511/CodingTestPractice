aad2db8f24c1d712750ac6b7b58daad65b09f5a4
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-02-06 22:42:44 +0900

    [Gold V] Title: 치킨 배달, Time: 196 ms, Memory: 16716 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,77 +1,75 @@
-import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
+import java.io.*;
 class Pair{
     int x;
     int y;
     public Pair(int x, int y){
-        this.x= x;
-        this.y= y;
+        this.x = x;
+        this.y = y;
     }
 }
-
 public class Main {
-    static int N,M;
+    static int n,m;
+    static int dist=Integer.MAX_VALUE;
     static int[][] map;
-
-    static ArrayList<Pair> list = new ArrayList<>();
-    static ArrayList<Pair> house = new ArrayList<>();// 치킨집 m개 찾을때 사용
-    static boolean[] checkChicken; // 치킨집 m개 찾을때 사용
-    static int min = Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException {
+    static ArrayList<Pair> chicken = new ArrayList<>();
+    static ArrayList<Pair> house = new ArrayList<>();
+    static boolean[] check;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
 
-        map = new int[N][N];
-
-
-        for(int i =0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<N; j++){
+        for(int i =0; i<n; i++){
+            st =new StringTokenizer(br.readLine());
+            for(int j =0; j<n; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] ==2){
-                    list.add(new Pair(i,j)); //현재 치킨집 좌표 저장
-                }else if(map[i][j] ==1){
+                if(map[i][j] ==1) {
                     house.add(new Pair(i,j));
                 }
+                else if(map[i][j]==2){
+                    chicken.add(new Pair(i,j));
+                }
             }
         }
-
-        //치킨집 개수 M보다 작거나 같으면 바로 치킨 거리 최소값 구하기
-        //M보다 크다면 치킨집 M개로 만들어준 다음 치킨 거리 최소값 구하기
-
-            checkChicken = new boolean[list.size()];
-            pickChicken(0,0);
-
-        System.out.println(min);
+        check = new boolean[chicken.size()];
+        findChicken(0,0);
+        //치킨집 n개 만큼 뽑고
+        //그 치킨집 최소 거리 구해서 저장
+        System.out.println(dist);
 
     }
-    public static void pickChicken(int start,int depth){
-        if(depth == M){
-            //치킨 거리 최솟값 구하기
-            int total = 0;
+    public static void findChicken(int start,int depth){
+        if(depth == m){
+            int hap=0;
+            //거리 계산
             for(int i =0; i<house.size(); i++){
-                int sum = Integer.MAX_VALUE; //현재 집에서 치킨집까지의 거리 최소
-                for(int j =0; j<list.size(); j++){
-                    if(checkChicken[j]){
-                        int dist = Math.abs(house.get(i).x-list.get(j).x) + Math.abs(house.get(i).y - list.get(j).y);
-                        sum = Math.min(sum,dist);
+                Pair cur = house.get(i);
+                int curX = cur.x;
+                int curY = cur.y;
+                int sum = Integer.MAX_VALUE; // 현재 집에서 치킨집까지 거리
+                for(int j =0; j<chicken.size(); j++){
+                    if(check[j]){
+                        Pair chick = chicken.get(j);
+                        int temp = Math.abs(curX-chick.x) + Math.abs(curY-chick.y);
+                        sum = Math.min(sum,temp);
                     }
                 }
-                total +=sum;
+                hap += sum;
             }
-            min = Math.min(min,total); //M개 조합의 치킨 집 중 최소 거리 저장
+            dist = Math.min(dist,hap);
+            return;
         }
-        for(int i =start; i<list.size(); i++){
-            if(!checkChicken[i]){
-                checkChicken[i] = true;
-                pickChicken(i+1,depth+1);
-                checkChicken[i] = false;
-            }
 
+        for(int i =start; i<chicken.size(); i++){
+            if(!check[i]){
+                check[i] = true;
+                findChicken(i+1,depth+1);
+                check[i] =false;
+            }
         }
     }
 

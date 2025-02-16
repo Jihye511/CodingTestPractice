e67b2f5ecd26739fa2e92367e1ba1adb336a19c2
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-02-16 17:01:54 +0900

    [level 2] Title: 프로세스, Time: 8.05 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,34 +2,35 @@ import java.io.*;
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        Queue<int[]> q= new ArrayDeque<>();
+       Queue<int[]> q = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i =0; i<priorities.length; i++){
-            q.offer(new int[]{i, priorities[i]});
+            q.offer(new int[]{priorities[i],i});
         }
-       ArrayList<Integer> idx = new ArrayList<>();
         
-        //원소 빼기 실행
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            //cur값과 q안 원소값 비교 
-            boolean check=false;
-            for(int[] next:q){
-                if(cur[1] < next[1]){
-                    check =true;// cur 값 다시 집어넣어야함
-                    break;
+            boolean check = false;
+            for(int i =0; i<q.size(); i++){
+                int[] next = q.poll();
+                if(cur[0] < next[0]){
+                    check = true;
                 }
+                q.offer(new int[] {next[0], next[1]});
             }
             if(check){
-                q.offer(new int[]{ cur[0],cur[1]});
+                q.offer(new int[]{cur[0],cur[1]});                
             }else{
-                idx.add(cur[0]);
+                list.add(cur[1]);
+            }
+            
+        }
+        int ans = 0;
+        for(int i =0; i<list.size(); i++){
+            if(list.get(i) ==location){
+                ans = i+1;
             }
         }
-        //idx값 location 값 비교 ㅎㅐ서 그 인덱스 찾기
-        for(int i =0; i<idx.size(); i++){
-            if(idx.get(i) == location) answer = i+1;
-        }
-        return answer;
+        return ans;
     }
 }

53b7bc6317f520ffb8222e2d539f046d3d5677f3
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-03-06 16:44:35 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 99 ms, Memory: 27,540 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -1,73 +1,80 @@
 
 import java.io.*;
 import java.util.*;
-
 public class Solution {
-    static int N, K, maxDepth, maxHeight;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+	static int N,K;
+	static int[][] map;
+	static int max;
+	static int maxResult;
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T =Integer.parseInt(br.readLine());
+		for(int t =1; t<=T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			
+			map = new int[N][N];
+			max = 0;
+			maxResult=0;
+			for(int i =0; i<N; i++) {
+				st= new StringTokenizer(br.readLine());
+				for(int j =0; j<N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					max = Math.max(max, map[i][j]);
+				}
+			}
+			
+			for(int i =0; i<N; i++) {
+				for(int j =0; j<N; j++) {
+					if(map[i][j]==max) {
+						boolean[][] v= new boolean[N][N];
+						v [i][j] =true;
+						dfs(i,j,true, 1,map,v);
+					}
+				}
+			}
+			System.out.println("#"+t + " " + maxResult);
+			
+		}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+	}
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	public static void dfs(int x, int y, boolean cut, int depth, int [][] temp,boolean[][]v) {
+		maxResult = Math.max(maxResult, depth);
+//		for(int i=0; i<N; i++) {
+//			for(int j =0; j<N; j++) {
+//				System.out.print(map[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+		for(int i =0; i<4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx<0 || nx>=N || ny<0 || ny>=N)continue;
+			if(v[nx][ny]) continue;
+			v[nx][ny] =true;
+			//그냥 가능하면 가라
+			if(temp[nx][ny]<temp[x][y]) dfs(nx, ny, cut, depth+1,temp,v);
+			
+			//불가능하면 깎을 수 있나 확인
+			else {
+				if(cut) {
+					for(int d =1; d<=K; d++) {
+						if(temp[nx][ny]-d <temp[x][y]) {
+							temp[nx][ny] -= d;
+							dfs(nx, ny, !cut, depth+1,temp,v);
+							temp[nx][ny] += d;
+						}
+					}
+				}
+				
+			}
+			v[nx][ny] =false;
+		}
+		
+	}
 
-        for (int t = 1; t <= T; t++) {
-            maxHeight = 0;
-            maxDepth = 0;
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-
-            map = new int[N][N];
-            visited = new boolean[N][N];
-
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                    maxHeight = Math.max(maxHeight, map[i][j]);
-                }
-            }
-
-            
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (map[i][j] == maxHeight) { 
-                        visited = new boolean[N][N];
-                        dfs(i, j, map[i][j], 1, false);
-                    }
-                }
-            }
-
-            System.out.println("#" + t + " " + maxDepth);
-        }
-    }
-
-    public static void dfs(int x, int y, int height, int depth, boolean cutUsed) {
-        maxDepth = Math.max(maxDepth, depth);
-        visited[x][y] = true;
-
-        for (int d = 0; d < 4; d++) {
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N || visited[nx][ny]) continue;
-
-            if (map[nx][ny] < height) { // 그냥 이동 가능
-                dfs(nx, ny, map[nx][ny], depth + 1, cutUsed);
-            } else if (!cutUsed) { 
-                for (int k = 1; k <= K; k++) { // K의 깊이만큼 깎아보기
-                    if (map[nx][ny] - k < height) {
-                        map[nx][ny] -= k; // 깎기
-                        dfs(nx, ny, map[nx][ny], depth + 1, true);
-                        map[nx][ny] += k;
-                    }
-                }
-            }
-        }
-        visited[x][y] = false;
-    }
 }

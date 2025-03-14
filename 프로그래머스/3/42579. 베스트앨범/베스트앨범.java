abcf0ca351d544120b3872890da671ebb0c85aa0
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-03-14 21:57:22 +0900

    [level 3] Title: 베스트앨범, Time: 4.94 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,45 +1,42 @@
 import java.io.*;
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        ArrayList<Integer> answer_list = new ArrayList<>();
         
-        //장르별 총 갯수
-        HashMap<String, Integer> map = new HashMap<>();
+        //1. 장르별 재생횟수
+        HashMap<String, Integer> genrePlayCount = new HashMap<>();
+        // 2. 장르별 노래 리스트 (각 노래의 (인덱스, 재생 횟수) 저장)
+        HashMap<String, ArrayList<int[]>> genreSongs = new HashMap<>();
+        
+        //데이터 담기
         for(int i =0; i<genres.length; i++){
-             map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
-        }
-        List<String> keys = new ArrayList<>(map.keySet());
-    
-		Collections.sort(keys, (v1, v2) -> (map.get(v2).compareTo(map.get(v1)))); 
-        
-        //큰 앨범 순으로 반복문(pop- classic)
-        for (String genre : keys) {
-            // 해당 장르의 노래들 중에서 idx와 plays 저장
-            HashMap<Integer, Integer> partMap = new HashMap<>();
-            for (int j = 0; j < genres.length; j++) {
-                if (genres[j].equals(genre)) {
-                    partMap.put(j, plays[j]);  // 인덱스와 재생 횟수 저장
-                }
-            }
-            // partMap에서 값이 큰 상위 2개의 인덱스 찾기
-            if(partMap.size()==1){
-                answer_list.add(partMap.keySet().iterator().next());
+            if(genrePlayCount.containsKey(genres[i])){
+                genrePlayCount.put(genres[i], genrePlayCount.get(genres[i]) + plays[i]); 
             }else{
-                partMap.entrySet()
-                    .stream()
-                    .sorted((e1, e2) -> e2.getValue() - e1.getValue())  // value 값을 기준으로 내림차순 정렬
-                    .limit(2)  // 상위 2개의 요소만 선택
-                    .forEach(e -> answer_list.add(e.getKey()));  // 인덱스(키)를 ArrayList에 추가
+                genrePlayCount.put(genres[i],plays[i]); 
             }
-            
+             genreSongs.putIfAbsent(genres[i], new ArrayList<>());
+            genreSongs.get(genres[i]).add(new int[]{i, plays[i]});
         }
         
-        int[] answer = new int[answer_list.size()];
-        for (int i = 0; i < answer_list.size(); i++) {
-            answer[i] = answer_list.get(i);
+        //장르를 재생횟수로 정렬
+        ArrayList<String> keySet = new ArrayList<>(genrePlayCount.keySet());
+        keySet.sort((o1, o2) -> genrePlayCount.get(o2).compareTo(genrePlayCount.get(o1)));
+        
+        List<Integer> bestAlbum = new ArrayList<>();
+       for(String genre : keySet){
+           //재생횟수 , 인덱스 기준으로 정렬
+           List<int[]> songs = genreSongs.get(genre);
+           songs.sort((a,b) -> b[1] !=  a[1] ? b[1] -a[1] : a[0] -b[0]);
+           
+           for(int i =0; i<Math.min(songs.size(),2); i++){
+               bestAlbum.add(songs.get(i)[0]);
+           }
         }
-        return answer;
+        int[] ans = new int[bestAlbum.size()];
+        for(int i =0; i<bestAlbum.size(); i++){
+            ans[i] = bestAlbum.get(i);
+        }
+        return ans;
     }
 }

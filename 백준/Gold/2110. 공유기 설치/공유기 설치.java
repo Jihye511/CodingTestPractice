588a1a006e80fc89df011a1d67514e17f08681f1
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-03-19 11:21:19 +0900

    [Gold IV] Title: 공유기 설치, Time: 264 ms, Memory: 29132 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,57 +1,54 @@
+
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
-
-
 public class Main {
-    static int N,C;
-    static int[] arr;
-    static boolean[] check;
+	static int N,C;
+	static int[] num;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		num = new int[N];
+		for(int i =0; i<N; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(num);
+		System.out.println(binary());
+				
+		
 
-    static int result = Integer.MIN_VALUE;
-    static Queue<Integer> q = new LinkedList<>();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
-        check = new boolean[N];
-        for(int i =0; i<N; i++){
-            arr[i] =Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(arr);
-        //이분탐색
-        System.out.println(binary());
-
-    }
-    public static int binary(){
-        int start = 1;
-        int end = arr[N-1]- arr[0]+1;
-        while(start<end){
-            int mid = (start+end)/2;
-            if(checkNum(mid) <C){
-                //공유기가 지정된 c보다 적으면 거리 줄여야됨
-                end = mid;
-            }else{
-                //크거나 같으면 거리 줄이기
-                start = mid+1;
-            }
-        }
-        return start-1;
-    }
-    public static int checkNum(int dist){
-        int before=arr[0];
-        int count=1;
-        for(int i =1; i<N; i++){
-            if(arr[i] -before >= dist){
-                count++;
-                before = arr[i];
-            }
-        }
-        return count;
-    }
+	}
+	public static int binary() {
+		int start = 1;
+		int end = num[N-1] - num[0]+1;
+		
+		while(start< end) {
+			int mid =(start + end)/2;
+			
+			if(checknum(mid)>=C) {
+				start = mid +1;
+			}else {
+				end = mid;
+			}
+		}
+		return start-1;
+		
+	}
+	public static int checknum(int m) {
+		
+		int before = num[0];
+		int cnt=1;
+		for(int i =1; i<N; i++) {
+			if(num[i]-before >=m) {
+				cnt++;
+				before = num[i];
+			}
+		}
+		
+		return cnt;
+	}
 
 }

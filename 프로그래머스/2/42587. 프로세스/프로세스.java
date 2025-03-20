2a36e9deaa30949d42b207530434bd366b834081
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-03-20 15:55:47 +0900

    [level 2] Title: 프로세스, Time: 1.45 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,35 +2,52 @@ import java.io.*;
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-       Queue<int[]> q = new LinkedList<>();
+//         Queue<int[]> pq = new PriorityQueue<>((a, b)->{
+//             if(a[0]==b[0]) return a[1]-b[1];
+//             else return b[0]-a[0];
+//         }); 
+        
+        Queue<int[]> q = new LinkedList<>();
         ArrayList<Integer> list = new ArrayList<>();
+        
         for(int i =0; i<priorities.length; i++){
             q.offer(new int[]{priorities[i],i});
+            // pq.offer(new int[]{priorities[i],i});
+            list.add(priorities[i]);
         }
+        Collections.sort(list, Collections.reverseOrder());
         
+        int cnt=1;
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            boolean check = false;
-            for(int i =0; i<q.size(); i++){
-                int[] next = q.poll();
-                if(cur[0] < next[0]){
-                    check = true;
+            
+            if(cur[0] == list.get(0)){
+                if(cur[1] == location){
+                    break;
                 }
-                q.offer(new int[] {next[0], next[1]});
-            }
-            if(check){
-                q.offer(new int[]{cur[0],cur[1]});                
-            }else{
-                list.add(cur[1]);
+                list.remove(0);
+                cnt++;
+            }else {
+                q.offer(new int[]{cur[0],cur[1]});
             }
             
         }
-        int ans = 0;
-        for(int i =0; i<list.size(); i++){
-            if(list.get(i) ==location){
-                ans = i+1;
-            }
-        }
-        return ans;
+        
+        
+        // Collections.sort(q,)
+        // int[] n = new int[priorities.length];
+        // int idx=0;
+        // for(int[] v: pq){
+        //     n[idx] =v[1];
+        //     idx++;
+        // }
+//         for(int i=0; i<priorities.length;i++){
+//             int[] cur=pq.poll();
+//             if(cur[1] == location){
+//                 return i+1;
+//             }
+//         }
+        
+        return cnt;
     }
 }

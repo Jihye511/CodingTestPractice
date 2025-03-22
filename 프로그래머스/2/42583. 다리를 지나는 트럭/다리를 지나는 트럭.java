37f89bfe6970ba8237700558607f723ddb1e217e
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-03-22 14:21:50 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 22.62 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,44 +3,41 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
         Queue<Integer> q = new LinkedList<>();
-        //런타임에러발생
-        // //비어잇는 다리 공간 모두 0으로 초기화
-        // for(int i =0; i<bridge_length;i++){
-        //     q.offer(0);
-        // }
+        int hap=0;
+        int cnt=0;
+        if(truck_weights.length ==1){
+            return bridge_length+1;
+        }
+        if(bridge_length==1){
+            return truck_weights.length+1;
+        }
         
-        if(bridge_length ==1) return truck_weights.length+1;
-        if(truck_weights.length == 1) return bridge_length+1;
-        
-        int curWeight =0;
-        for(int t:truck_weights){
+        for(int t : truck_weights){
             while(true){
                 if(q.isEmpty()){
                     q.offer(t);
-                    curWeight +=t;
-                    answer ++;
+                    cnt++;
+                    hap+=t;
                     break;
+                }else if(q.size()==bridge_length){
+                    //빼기
+                    hap -=q.poll();
                 }
-                else if(q.size()==bridge_length){
-                    curWeight -=q.poll();
-                }
-                else {
-                    if(curWeight +t >weight){
+                else{
+                    if(hap+t> weight){
                         q.offer(0);
-                        answer ++;
-                    }
-                    else{
+                        cnt++;
+                    }else{
                         q.offer(t);
-                        curWeight +=t;
-                        answer ++;
+                        hap+=t;
+                        cnt++;
                         break;
                     }
                 }
             }
         }
-        
-        return answer+bridge_length;
+        cnt+= bridge_length;
+        return cnt;
     }
 }

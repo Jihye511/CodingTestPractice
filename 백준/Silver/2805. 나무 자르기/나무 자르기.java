bf97d4e8ae5861ae19319824e3640f1f0a77c2cf
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-03-22 17:30:07 +0900

    [Silver II] Title: 나무 자르기, Time: 912 ms, Memory: 119416 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,52 +1,44 @@
 import java.io.*;
 import java.nio.Buffer;
 import java.util.*;
-public class Main {
-    static int N;
-    static Long M;
-    static int[] trees;
 
-    public static void main(String[] args) throws IOException {
+public class Main {
+    static int N,M;
+    static int[] tree;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Long.parseLong(st.nextToken());
-
-        trees = new int[N];
+        N =Integer.parseInt(st.nextToken());
+        M =Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(trees);
-
-        System.out.println(binarySearch(M));
-    }
-    public static long binarySearch(long m ){
-        long start = 0;
-        long end = trees[N-1];
-        long result =0;
-        while(start<= end){
-            long mid= (start + end)/2;
-            long extra = calculate(mid);
-            if(extra >=m){
-                result = mid;
-                start = mid+1;
-            }else{
-                end = mid -1;
-            }
-        }
-        return result;
-    }
-    public static long calculate(long m){
-        long sum=0;
+        tree =new int[N];
         for(int i =0; i<N; i++){
-            if(trees[i]>m){
-                sum += trees[i]-m;
+            tree[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(tree);
+        System.out.println(upperbound());
+    }
+    public static long cutTree(long size){
+        long extra =0;
+        for(int t : tree){
+            if(t<=size) continue;
+            extra += (t-size);
+        }
+        return extra;
+    }
+    public static long upperbound(){
+        long lo = 0;
+        long hi = tree[N-1];
+        long answer=0;
+        while(lo<=hi){
+            long mid = lo +(hi-lo)/2;
+            if(cutTree(mid) >=M){
+                answer = mid;
+                lo = mid+1;
+            }else{
+                hi = mid-1;
             }
         }
-        return sum;
+        return answer;
     }
-
-
 }

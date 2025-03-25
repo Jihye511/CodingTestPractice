b1f0a4a207aba7030cbdbadd0e27a7ff6b5ba526
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-03-25 19:42:13 +0900

    [Gold II] Title: 문제집, Time: 428 ms, Memory: 43364 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,51 +1,51 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
+public class Main {
     static int N,M;
     static ArrayList<Integer>[] list;
-    static int[] indegree;
-    static int[] result;
+    static int[] num;
     static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        indegree = new int[N+1];
+        num = new int[N+1];
         list = new ArrayList[N+1];
-        result = new int[N+1];
-        for(int i =1; i<N+1; i++){
+        for(int i =1; i<=N; i++){
             list[i] = new ArrayList<>();
         }
-        //입력받기
-        for(int i =0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-            int pre = Integer.parseInt(st.nextToken());
-            int num = Integer.parseInt(st.nextToken());
-            list[pre].add(num);
-            indegree[num]++;
+        for(int i =0;i<M; i++){
+            st= new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b= Integer.parseInt(st.nextToken());
+
+            list[a].add(b);
+            num[b]++;
+
         }
-        topologicalSort();
+        bfs();
         System.out.println(sb);
+
     }
-    public static void topologicalSort(){
-        Queue<Integer> q = new PriorityQueue<>();
-        for(int i=1; i<N+1; i++){
-            if(indegree[i] ==0){
+    public static void bfs(){
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for(int i =1; i<=N; i++){
+            if(num[i]==0){
                 q.offer(i);
             }
         }
-        while (!q.isEmpty()){
-            int current = q.poll();
-            sb.append(current).append(" ");
-            for(int next : list[current]){
-                indegree[next] --;
-                if(indegree[next] ==0) {
-                    q.add(next);
-
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            sb.append(cur).append(" ");
+            for(int i =0; i<list[cur].size(); i++){
+                int next = list[cur].get(i);
+                num[next]--;
+                if(num[next] ==0){
+                    q.offer(next);
                 }
             }
         }

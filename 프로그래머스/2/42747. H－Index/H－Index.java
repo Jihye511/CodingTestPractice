67d28b021915cf3b451c21a615ab1df32328a77b
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-04-01 12:56:31 +0900

    [level 2] Title: H-Index, Time: 1.94 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,16 +1,24 @@
 import java.io.*;
 import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         Arrays.sort(citations);
-        for(int i =0; i<citations.length; i++){
-            int h = citations.length-i; //인용된 논문의 수
-            if(citations[i]>=h) {
-                answer=h;
-                break;
+        
+        for(int i =0; i<citations[citations.length-1]; i++){
+            int idx =0;
+            for(int j=0; j<citations.length; j++){
+                if(citations[j]>= i){
+                    idx =j;
+                    break;
+                }
             }
+            
+            int size = citations.length -idx;
+            if(size >= i){
+                answer =i;        
+            }else break;
+            
         }
         return answer;
     }

86a605e6af2c8b3bf19b78c031740100daabf3d8
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-04-20 13:53:24 +0900

    [level 3] Title: 섬 연결하기, Time: 1.10 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,35 +1,51 @@
+import java.io.*;
 import java.util.*;
+
+class Node{
+    int end;
+    int cost;
+    public Node(int end, int cost){
+        this.end = end;
+        this.cost = cost;
+    }
+}
 class Solution {
     static PriorityQueue<int[]> pq;
-    static List<int[]> list[];
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        list = new ArrayList[n];
-        for(int i=0; i<n; i++)
-            list[i]=new ArrayList<>();
-        pq = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[] o, int[] o2){
-                return o[1] - o2[1];
-            }
-        });
-        for(int[] cost: costs){
-            list[cost[0]].add(new int[]{cost[1], cost[2]});
-            list[cost[1]].add(new int[]{cost[0], cost[2]});
+        ArrayList<Node>[]connect = new ArrayList[n];
+        for(int i =0; i<n; i++){
+            connect[i] = new ArrayList<>();
         }
-        boolean visited[] = new boolean[n];
-        pq.add(new int[]{0,0});
+        for(int i =0; i<costs.length; i++){
+            int s = costs[i][0];
+            int e = costs[i][1];
+            connect[s].add(new Node(e, costs[i][2]));
+            connect[e].add(new Node(s, costs[i][2]));
+        }
+        //거리 기준 정렬
+        pq = new PriorityQueue<>((a,b)->(a[1]-b[1]));
+        
+        boolean[]v = new boolean[n];
+        pq.offer(new int[]{0,0}); //(도착지, 비용)
         while(!pq.isEmpty()){
-            int[] arr = pq.poll();
-            if(visited[arr[0]]) continue;
-            visited[arr[0]] = true;
-            answer += arr[1];
-            
-            for(int node[]: list[arr[0]]){
-                if(!visited[node[0]]){
-                    pq.add(new int[]{node[0], node[1]});
+            int[] cur = pq.poll();
+            if(!v[cur[0]]){
+                v[cur[0]]=true;
+                answer+= cur[1];
+                for(int i =0; i< connect[cur[0]].size(); i++){
+                    Node next = connect[cur[0]].get(i);
+                    if(!v[next.end]){
+                        pq.offer(new int[]{next.end, next.cost});
+                    }
                 }
             }
         }
+        
+        
+       
+        
+        
         return answer;
     }
 }

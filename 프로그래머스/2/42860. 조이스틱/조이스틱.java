cb9e54a372d1d6c0c50ea5f74b0e6558f57583a1
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-04-20 15:29:05 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,26 +1,29 @@
 import java.io.*;
 import java.util.*;
 class Solution {
-    
     public int solution(String name) {
-        int result = 0;
-        int move = name.length() - 1;
-        
+        int answer = 0;
+        int idx =0;
+        int move =name.length()-1;
+        boolean[] v = new boolean[name.length()];
         for(int i =0; i<name.length(); i++){
-            result += Math.min(name.charAt(i) - 'A', 26 - (name.charAt(i) - 'A') );
+            char c = name.charAt(i);
+            //위아래 움직이기
+            answer += Math.min(c - 65, 91-c);
+        
             
-            //연속된 'A' 가 끝나는 지점 찾기
+            //A가 끝나는 지점 찾기
             int next = i+1;
-            while(next<name.length() && name.charAt(next) =='A'){
-                next++;
+            while(next<name.length() && name.charAt(next)=='A'){
+                next ++;
             }
             
-            //좌우이동 최소 횟수 구하기(순서대로 va 뒤로 돌아가기)
-            move = Math.min(move, (i*2) + name.length() - next);
-            move = Math.min(move, (name.length()-next)*2 +i);
+            //좌우이동 최소횟수 구하기
+            move = Math.min(move, Math.min(i*2 + name.length()-next , (name.length()-next)*2 +i));
         }
-        result +=move;
-        return result;
+        
+        answer +=move;
+        
+        return answer;
     }
-    
 }

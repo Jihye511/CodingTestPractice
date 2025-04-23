9b4d6b5735075ce4942420d7cbbe0179334a9b5d
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-04-23 20:18:53 +0900

    [level 4] Title: 도둑질, Time: 28.11 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,19 +1,19 @@
 import java.io.*;
 import java.util.*;
+
 class Solution {
     public int solution(int[] money) {
-        int[] Include0 = new int[money.length];
-        int[] Exclude0 = new int[money.length];
+        int answer = 0;
+        int size = money.length;
+        int[] includeZero = new int[size];
+        int[] excludeZero = new int[size];
+        includeZero[0] =includeZero[1]= money[0];
+        excludeZero[1] = money[1];
         
-        //0번째 포함
-        Include0[0] = Include0[1] = money[0];
-        //불포함
-        Exclude0[1] = money[1];
-        
-        for(int i =2; i<money.length; i++){
-            Include0[i] = Math.max(Include0[i-2] +money[i], Include0[i-1]);
-            Exclude0[i] = Math.max(Exclude0[i-2] + money[i], Exclude0[i-1]);
+        for(int i =2; i<size; i++){
+            includeZero[i] = Math.max(includeZero[i-2] + money[i], includeZero[i-1]);
+            excludeZero[i] = Math.max(excludeZero[i-2]+money[i], excludeZero[i-1]);
         }
-        return Math.max(Include0[money.length-2],Exclude0[money.length-1]);
+        return Math.max(includeZero[size-2], excludeZero[size-1]);
     }
 }

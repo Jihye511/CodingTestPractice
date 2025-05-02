b56fd5f1b6aa0eb5f3d10b32ee48da77f66327b6
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-05-02 15:44:08 +0900

    [level 3] Title: N으로 표현, Time: 44.24 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,41 +2,83 @@ import java.io.*;
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        //통만들기
-        List<HashSet<Integer>> list = new ArrayList<HashSet<Integer>>();
-        //총 8개통 만들기 (i는 쓰인 숫자의 개수)
-        for(int i =0; i<=8; i++){
-            list.add(new HashSet<Integer>());
+        int answer = -1;
+        if(N==number) return 1;
+        HashSet<Integer>[] set = new HashSet[9];
+        for(int i=0; i<9; i++){
+            set[i] = new HashSet<>();
         }
-        list.get(1).add(N);
-        if(number ==N) return 1;
+        boolean check=false;
+        int temp =N;
+        ArrayList<Integer>[] templist = new ArrayList[9];
+        for(int i=0; i<9; i++){
+            templist[i] = new ArrayList<>();
+        }
+        for(int i =1; i<9; i++){            
+            templist[i].add(temp); // 5555...
+            set[i].add(temp);
+            temp = temp *10 +N;
+        }
         
-        for(int i =2; i<=8; i++){
-            //total = 숫자를 i개 썼을 떄 숫자들이 들어갈 통
-            HashSet<Integer> total = list.get(i);
-            
-            for(int j=1; j<i; j++){
-                HashSet<Integer> a = list.get(j);
-                HashSet<Integer> b = list.get(i-j);
+        //5555.. 끼리 계산
+        for(int i =1; i<9; i++){
+            for(int j =1; j<i; j++){
+                int a = templist[j].get(0);
+                int b = templist[i-j].get(0);
+                // System.out.println("a : " + a + " b: "+b);
+                set[i].add(a-b);
+                set[i].add(b-a);
+                set[i].add(a/b);
+                set[i].add(b/a);
+                set[i].add(a+b);
+                set[i].add(a*b);
                 
-                for(int x :a){
-                    for(int y:b){
-                        total.add(x+y);
-                        total.add(x-y);
-                        total.add(x*y);
-                        if(x!=0&&y!=0) total.add(x/y);
+            }
+        }
+        
+        for(int i =2; i<9; i++){
+            for(int e =1; e<i; e++){
+                ArrayList<Integer> first = new ArrayList<>(set[e]);//전 꺼
+                ArrayList<Integer> second = new ArrayList<>(set[i-e]);//전 꺼
+                ArrayList<Integer> cur = new ArrayList<>(); 
+                for(int j =0; j<first.size(); j++){
+                    for(int k =0; k<second.size(); k++){
+                        cur.add(first.get(j) + second.get(k));
+                        //빼기,나누기
+
+                        cur.add(first.get(j) - second.get(k));
+                        
+                        if(first.get(j) !=0){
+                            cur.add(second.get(k)/first.get(j));
+                        }
+
+                        
+                        //곱하기
+                        cur.add(first.get(j) * second.get(k));        
+                    }            
+                }
+                for(int j=0; j<cur.size(); j++){
+
+                    set[i].add(cur.get(j));
+                }
+                
+                for(int d : set[i]){
+                    if(d == number){
+                        check = true;
+                        break;
                     }
                 }
-                //같은 숫자로 된거 추가
-                //ex> 222같은거//
-                total.add(Integer.parseInt(String.valueOf(N).repeat(i)));
-            }
             
-            if(total.contains(number)) return i;
+                if(check){
+                    break;
+                }
+            }
+            if(check){
+                    answer = i;
+                    break;
+                }
+        
         }
-        
-        
-    
-        return -1;
+        return answer;
     }
 }

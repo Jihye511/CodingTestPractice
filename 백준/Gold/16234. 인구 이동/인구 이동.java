cfc2bc357001f90a6bb9c8be4b5b835f14e673a2
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-09-05 09:51:49 +0900

    [Gold IV] Title: 인구 이동, Time: 576 ms, Memory: 298088 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,116 +1,76 @@
-
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int N,L,R;
-	static int[][] map;
-	static int [][] group;
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		map= new int[N][N]; //인구수 저장
-		group = new int[N][N]; //그룹 넘버 저장
-		
-		for(int i= 0; i<N; i++) {
-			st= new StringTokenizer(br.readLine());
-			for(int j =0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		int day=0;
-		while(true) {
-			
-			//전체 탐색하면서 그룹핑하기
-			int n=1;
-			group = new int[N][N];
-			for(int i= 0; i<N; i++) {
-				for(int j =0; j<N; j++) {
-					if(group[i][j]==0) {
-						//n을 넣어서 같은 숫자는 같은 그룹으로 묶기
-						grouping(i, j, n);
-						n++;
-					}
-				}
-			}
-			if(n== N*N+1) {
-				//국경선 아무데도 안열렸음
-				break;
-			}
-			day++;
-//			for(int i =1; i<n; i++) {
-//				move(i);
-//			}
+    static int N, L, R;
+    static int[][] map;
+    static int day;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j =0; j<N; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        day = 0;
+        while(true){
+            //1. 인구 차 계산 이동가능 확인
+            boolean[][] v = new  boolean[N][N];
+            int t =0;
+            for(int i =0; i<N; i++){
+                for(int j =0; j<N; j++){
+                    if(!v[i][j]){
+                        v[i][j] = true;
+                        t +=generate(v,i,j);
+                    }
+                }
+            }
+            if(t == 0) break;
+            //3. day++
+            day++;
+        }
+        System.out.println(day);
+    }
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    public static int generate(boolean[][] v, int n, int m){
+        Queue<int[]> q = new LinkedList<>();
+        int hap =0;
+        q.offer(new int[]{n,m});
+        ArrayList<int[]> list = new ArrayList<>();
+        list.add(new int[]{n,m});
+        hap += map[n][m];
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            for(int i =0; i<4; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if(nx<0 || ny<0 || nx>=N || ny>=N)  continue;
+                if(v[nx][ny])continue;
 
-		}
-		System.out.println(day);
-	}
+                int val = Math.abs(map[cur[0]][cur[1]] - map[nx][ny]);
+                if(L<=val && R>=val){
+                    v[nx][ny] = true;
+                    q.offer(new int[]{nx,ny});
+                    list.add(new int[]{nx,ny});
+                    hap+=map[nx][ny];
+                }
+            }
+        }
 
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	//국경선 열린 거 확인
-	public static void grouping(int x, int y,int n) {
-		Queue<int[]> q = new LinkedList<>();
-		ArrayList<int[]> list = new ArrayList<>();
-		q.offer(new int[] {x,y});
-		group[x][y] =n;
-		int hap=0;
-		list.add(new int[] {x,y});
-		hap += map[x][y];
-		
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			for(int i =0; i<4; i++) {
-				int nx = cur[0] + dx[i];
-				int ny = cur[1] + dy[i];
-				
-				if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
-				if(group[nx][ny] != 0) continue; // 이미 다른 그룹으로 묶임 
-				
-				//현재 국가 인구랑 다음 국가 인구 비교
-				int now = map[cur[0]][cur[1]];
-				int next =map[nx][ny]; 
-				if(compared(now, next)) {
-					group[nx][ny] = n;
-					q.offer(new int[] {nx,ny});
-					list.add(new int[] {nx,ny});
-					hap += map[nx][ny];
-				}
-				
-			}
-		}
-		
-		//계산해서 넣기
-		for(int[] point: list) {
-			map[point[0]][point[1]] =(int)hap/list.size();
-		}
-		
-	}
-	//국경 열어도 되는 지 확인
-	public static boolean compared(int cur, int next) {
-		return (L<= Math.abs(cur-next)) && (Math.abs(cur-next)<=R);
-	}
-//	//인구 이동
-//	public static void move(int n) {
-//		int hap=0;
-//		ArrayList<int[]> list = new ArrayList<>();
-//		for(int i= 0; i<N; i++) {
-//			for(int j =0; j<N; j++) {
-//				if(group[i][j]==n) {
-//					list.add(new int[] {i,j});
-//					hap += map[i][j];
-//				}
-//			}
-//		}
-//		
-//		//계산해서 넣기
-//		for(int[] point: list) {
-//			map[point[0]][point[1]] =(int)hap/list.size();
-//		}
-//	}
+        for(int i=0; i<list.size(); i++){
+            int newval = hap/list.size();
+            int[]  cur =list.get(i);
+            map[cur[0]][cur[1]] = newval;
+        }
+        return list.size()>1 ? 1: 0;
+    }
+    public static void move(ArrayList<ArrayList<Integer>> list){
 
+    }
 }

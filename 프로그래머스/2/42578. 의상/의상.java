d31b03deb25c2e45aee0a51dd8cc8a9290a6a8ef
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-10-29 21:22:03 +0900

    [level 2] Title: 의상, Time: 0.18 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,20 +1,19 @@
 import java.io.*;
 import java.util.*;
+    
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1;
         HashMap<String, Integer> map = new HashMap<>();
-        HashSet<String> set = new HashSet<>();
-        for(int i =0;i<clothes.length; i++){
-            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
-            set.add(clothes[i][1]);
+        HashSet<String> list = new HashSet<>();
+        for(int i =0; i<clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1],0)+1);
+            list.add(clothes[i][1]);
         }
-        int cnt=1;
-        for (String key : set) {
+        for(String key : list){
             int n = map.get(key);
-            cnt *= (n + 1);
+            answer *= (n+1);
         }
-        answer = cnt-1;
-        return answer;
+        return answer-1;
     }
 }

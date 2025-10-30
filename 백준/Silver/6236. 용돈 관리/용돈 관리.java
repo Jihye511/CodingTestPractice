e94818099b2f3fe39a248b4819148f1f16fefe4a
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-10-30 19:48:47 +0900

    [Silver I] Title: 용돈 관리, Time: 232 ms, Memory: 22336 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,52 +1,52 @@
+import javax.imageio.ImageTranscoder;
 import java.io.*;
 import java.util.*;
-
 public class Main {
     static int N,M;
     static int[] money;
-    static int maxV = 0;
-    static int hap=0;
-    public static void main(String[] args) throws IOException{
+    static int hap;
+    static int min;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N =Integer.parseInt(st.nextToken());
-        M =Integer.parseInt(st.nextToken());
+        StringTokenizer st= new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         money = new int[N];
         for(int i =0; i<N; i++){
             money[i] = Integer.parseInt(br.readLine());
-            hap += money[i];
-            maxV = Math.max(maxV, money[i]);
+            min = Math.max(money[i], min);
+            hap+= money[i];
         }
-        System.out.println(binarySearch());
+        System.out.println(binary());
 
     }
-    public static long  Withdrawal(long maxMoney){
-        long tempday = maxMoney;
-        long cnt =1;
-        for(int m : money){
-            if(tempday-m >=0){
-                tempday-=m;
-            }else{
-                tempday = maxMoney-m; //인출하고 그날 쓴 돈 바로 빼기
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-    public static long binarySearch(){
-        long lo = maxV;
-        long hi = hap;
-        long result = 0;
+    public static int binary(){
+        int lo =min;
+        int hi = hap;
+        int ans =0;
         while(lo<=hi){
-            long mid = lo +(hi-lo)/2;
-            if(Withdrawal(mid) <= M){
-                result = mid;
-                hi = mid-1;
-            }else{
+            int mid =(hi-lo)/2 + lo;
+            if(countDays(mid)>M){
                 lo = mid+1;
+            }else{
+                ans = mid;
+                hi = mid-1;
             }
         }
-        return result;
+        return ans;
+    }
+    public static int countDays(int value){
+         int cnt =0;
+         int cur= 0;
+         for(int i : money){
+             if(i > cur){
+                 cur = value;
+                 cnt++;
+             }
+             cur -= i;
+
+         }
+         return cnt;
     }
 }

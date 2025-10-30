6ac41de674aa3d1efa904e53d87dc45ee1174ee5
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-10-30 16:41:23 +0900

    [level 2] Title: 올바른 괄호, Time: 19.33 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,32 +1,26 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Queue<Character> q =new LinkedList<>();
+        boolean answer = true;
+        Queue<Character> q = new LinkedList<>();
         char[] ch = new char[s.length()];
         for(int i =0; i<s.length(); i++){
-            ch[i] = s.charAt(i);
-            q.offer(ch[i]);
+            q.offer(s.charAt(i));
+            if(i ==0 && s.charAt(i) == ')') return false;
         }
-        if(q.peek() ==')') return false;
-        boolean check=false;
-        int num=0;
+        int cnt=0;
         while(!q.isEmpty()){
-            char c = q.poll();
-            if(c =='('){
-                num +=1;
-            }else{
-                num-=1;
-            }
+            char cur = q.poll();
+            if(cur =='(') cnt ++;
+            else if(cur == ')') cnt--;
             
-            if(num<0) return false;
-            
+            if(cnt<0) return false;
             
         }
-        if(num ==0) return true;
-        else return false;
-        
-      
+        if(cnt != 0) return false;
+
+        return true;
     }
 }

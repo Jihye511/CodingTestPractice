04593c0fd4b1bf3366f5b31f271ee46438ee1816
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-10-30 18:30:33 +0900

    [Gold IV] Title: 공유기 설치, Time: 252 ms, Memory: 29460 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,54 +1,43 @@
-
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int N,C;
-	static int[] num;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		num = new int[N];
-		for(int i =0; i<N; i++) {
-			num[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(num);
-		System.out.println(binary());
-				
-		
-
-	}
-	public static int binary() {
-		int start = 1;
-		int end = num[N-1] - num[0]+1;
-		
-		while(start< end) {
-			int mid =(start + end)/2;
-			
-			if(checknum(mid)>=C) {
-				start = mid +1;
-			}else {
-				end = mid;
-			}
-		}
-		return start-1;
-		
-	}
-	public static int checknum(int m) {
-		
-		int before = num[0];
-		int cnt=1;
-		for(int i =1; i<N; i++) {
-			if(num[i]-before >=m) {
-				cnt++;
-				before = num[i];
-			}
-		}
-		
-		return cnt;
-	}
-
+    static int N,C;
+    static int[] home;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N =Integer.parseInt(st.nextToken());
+        C =Integer.parseInt(st.nextToken());
+        home = new int[N];
+        for(int i =0; i<N; i++){
+            home[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(home);
+        System.out.println(binary());
+    }
+    public static int binary(){
+        int ans = 0;
+        int lo = 0;
+        int hi = home[N-1] - home[0]+1;
+        while(lo<hi){
+            int mid = (hi-lo)/2 + lo;
+            if(countHome(mid)<C){
+                hi = mid;
+            }else{
+                lo = mid+1;
+            }
+        }
+        return lo-1;
+    }
+    public static int countHome(int dist){
+        int cnt=1;
+        int h = 0;
+        for(int i=1; i<home.length; i++ ){
+            if (home[i] - home[h] >= dist) {
+                cnt++;
+                h = i;
+            }
+        }
+        return cnt;
+    }
 }

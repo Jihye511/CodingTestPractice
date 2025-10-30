1eed199b9305e19db6c98998a5e158178642a280
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-10-30 17:26:26 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 40.72 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,43 +1,44 @@
 import java.io.*;
 import java.util.*;
-
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> q = new LinkedList<>();
-        int hap=0;
-        int cnt=0;
-        if(truck_weights.length ==1){
-            return bridge_length+1;
-        }
-        if(bridge_length==1){
-            return truck_weights.length+1;
+        int answer = 0;
+        Queue<Integer> waiting = new LinkedList<>();
+        Queue<Integer> bridge = new LinkedList<>();
+        int total = 0;
+        for(int i : truck_weights){
+            waiting.offer(i);
+            total+=i;
         }
         
-        for(int t : truck_weights){
-            while(true){
-                if(q.isEmpty()){
-                    q.offer(t);
-                    cnt++;
-                    hap+=t;
-                    break;
-                }else if(q.size()==bridge_length){
-                    //빼기
-                    hap -=q.poll();
+        if(total <= weight){
+            return bridge_length + truck_weights.length;
+        }
+        int hap =0;
+    
+            while(!waiting.isEmpty()){
+                int cur = waiting.peek();
+                if(bridge.size() == bridge_length){
+                    int w = bridge.poll();
+                    hap -= w;
                 }
-                else{
-                    if(hap+t> weight){
-                        q.offer(0);
-                        cnt++;
+                //다리 아직 하나도 안올라감
+                if(bridge.size()< bridge_length){
+                    if(hap + cur <=weight){
+                        waiting.poll();
+                        bridge.offer(cur);
+                        hap += cur;
                     }else{
-                        q.offer(t);
-                        hap+=t;
-                        cnt++;
-                        break;
+                        bridge.offer(0);
                     }
                 }
+                
+                answer++;
             }
-        }
-        cnt+= bridge_length;
-        return cnt;
+            
+            answer += bridge.size();
+            
+        
+        return answer;
     }
 }

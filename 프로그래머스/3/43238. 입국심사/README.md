01f4d700c847b88a0b236dfd59094caa6fdbe6e0
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2025-10-31 11:54:32 +0900

    [level 3] Title: 입국심사, Time: 102.87 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 86.9 MB, 시간: 128.90 ms
+메모리: 87.6 MB, 시간: 102.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 14일 14:50:48
+2025년 10월 31일 11:54:29
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,32 +1,30 @@
 import java.io.*;
 import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
         Arrays.sort(times);
-        answer = binary(n, times);
-    
-        
+        answer = binary(n,times);
         return answer;
     }
+    //lowerbound
     public static long binary(int n, int[] times){
         long lo = 1;
-        long hi = (long)times[0] * (long)n;
+        long hi = (long) n* (long)times[times.length-1];
         while(lo<hi){
             long mid = (hi-lo)/2 + lo;
-            
-            if(countNum(mid, times)>=n){
-                hi = mid;
-            }
-            else{ 
+            if(count(mid, times) < n){
                 lo = mid+1;
+            }else{
+                hi = mid;
             }
         }
         return lo;
     }
-    public static long countNum(long time, int[] times){
-        long cnt=0;
-        for(int i =0;i<times.length; i++){
+    public static long count(long time, int[] times){
+        long cnt =0;
+        for(int i =0; i<times.length; i++){
             cnt += time/times[i];
         }
         return cnt;

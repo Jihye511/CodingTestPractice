a63d185c82750eaeaa8ccce8947ab2fdc759763e
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-03-10 16:21:35 +0900

    [Silver II] Title: 예산, Time: 156 ms, Memory: 15812 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,35 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import javax.imageio.ImageTranscoder;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int left=0, right=-1;
-        int[] arr = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            right = Math.max(right, arr[i]);
+public class Main {
+   public static void main(String[] args)throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringBuilder sb = new StringBuilder();
+        int N  = Integer.parseInt(br.readLine());
+        int[] city = new int[N];
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int sum=0;
+        for(int i =0; i<N; i++){
+            city[i] = Integer.parseInt(st.nextToken());
+            sum+= city[i];
         }
+       Arrays.sort(city);
+        int M = Integer.parseInt(br.readLine());
 
-        int m = Integer.parseInt(br.readLine());
-        while(left<=right) {
-            int mid = (left+right)/2;
-            long budget =0;
-            for(int i=0; i<n; i++) {
-                if(arr[i]>mid) budget += mid;
-                else budget+= arr[i];
-            }
-            if(budget<=m) {
-                left = mid+1;
-            }else {
-                right = mid-1;
+        if(sum<=M) {
+            System.out.println(city[N-1]);
+            return;
+        }
+        int lo = 1;
+        int hi = city[N-1];
+        int ans = 0;
+        while(lo<=hi){
+            int mid = (hi-lo)/2 + lo;
+            if(isEnough(city,M,mid)){
+                ans = mid;
+                lo = mid+1;
+            }else{
+                hi = mid-1;
             }
         }
-        System.out.println(right);
-    }
+       System.out.println(ans);
+
+
+
+   }
+   public static boolean isEnough(int[] city, int M, int mid){
+       long sum = 0;
+       for(int i:city){
+           if(i<mid) sum+=i;
+           else sum+=mid;
+       }
+       return sum<=M;
+   }
 }

623d9941c7c6b8fabc8f1ee437f344082da5d58d
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-03-11 21:47:43 +0900

    [Silver III] Title: 햄버거 분배, Time: 116 ms, Memory: 14948 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.java
@@ -1,31 +1,53 @@
+import javax.imageio.ImageTranscoder;
+import javax.swing.*;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+   public static void main(String[] args)throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st= new StringTokenizer(br.readLine());
+       int N  = Integer.parseInt(st.nextToken());
+       int K  =Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+       String str= br.readLine();
+       int count =0;
+       Queue<Integer> qP = new LinkedList<>(); //사람
+       Queue<Integer> qH = new LinkedList<>(); //햄버거
+       for(int i=0; i<str.length(); i++){
+           if(str.charAt(i)=='P'){
+               boolean eat =false;
+               if(!qH.isEmpty()){
+                   while(!qH.isEmpty()){
+                       int hamburger = qH.poll();
+                       if(hamburger >=i-K && hamburger<=i+K){
+                           count++;
+                           eat =true;
+                           break;
+                       }
+                   }
+                   if(!eat) qP.add(i);
+               }else  qP.add(i);
 
-        String str = br.readLine();
-        char[] arr = str.toCharArray();
-        boolean[] visited = new boolean[N];
-        int num=0;
-        for(int i =0; i<N; i++){
-            if(arr[i] =='H') continue;
-                for(int j =-K; j<=K; j++){
-                    int idx = i +j;
-                    if(idx <0 || idx>=N )continue;
-                    if(arr[idx] == 'H' && !visited[idx]){
-//                        System.out.println("i: " +i + " idx : " +idx);
-                        visited[idx] = true;
-                        num++;
-                        break;
-                    }
-                }
-            }
-        System.out.println(num);
-    }
 
+           }else{
+               //햄버거일때
+               boolean eat =false;
+               if(!qP.isEmpty()){
+                   while(!qP.isEmpty()){
+                       int h  = qP.poll();
+                       if(h>=i-K && h<=i+K){
+                           count++;
+                           eat = true;
+                           break;
+                       }
+                   }
+                   if(!eat) qH.offer(i);
+               }else qH.offer(i);
+
+           }
+       }
+       System.out.println(count);
+   }
 }

890216260ddabf525ac1e5ed148f5226d92f2b71
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-03-11 20:54:13 +0900

    [Silver II] Title: 수 이어 쓰기, Time: 136 ms, Memory: 16224 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -1,22 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import javax.imageio.ImageTranscoder;
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int num=1;
-        while(!str.isEmpty()){
-            String numStr = Integer.toString(num);
-            int idx =0; //str의 현재 위치
-            for(int i =0; i<numStr.length(); i++){
-                if(idx < str.length() && numStr.charAt(i) == str.charAt(idx)){
-                    idx++;
-                }
-            }
-            str = str.substring(idx); //앞부분 자르기
-            num++;
-        }
-        System.out.println(num-1);
-    }
+    static String str;
+   public static void main(String[] args)throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       str =br.readLine();
+       int idx=0;
+       int n =1;
+       while(idx<str.length()){
+           String numString = String.valueOf(n);
+           for(int i =0; i<numString.length(); i++){
+               if(numString.charAt(i) == str.charAt(idx)){
+                   idx++;
+                   if(idx==str.length()) break;
+               }
+           }
+           n++;
+       }
+       System.out.println(n-1);
+
+
+   }
 }

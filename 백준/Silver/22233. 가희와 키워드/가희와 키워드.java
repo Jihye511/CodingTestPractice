360954f5943741033ccef76fa36ae8a8edeba601
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-03-28 22:57:54 +0900

    [Silver III] Title: 가희와 키워드, Time: 1012 ms, Memory: 218320 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -1,36 +1,30 @@
+import javax.imageio.ImageTranscoder;
 import java.io.*;
 import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+   public static void main(String[] args)throws IOException {
+       StringBuilder sb = new StringBuilder();
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < N; i++){
-            map.put(br.readLine(), 1);
+        HashMap<String ,Integer> map = new HashMap<>();
+        for(int i =0; i<N; i++){
+            String str = br.readLine();
+            map.put(str, 1);
         }
-        StringBuilder sb = new StringBuilder();
-        int cnt=N;
-        for(int i = 0; i < M; i++){
-            String[] str = br.readLine().split(",");
-
-            for(int j = 0; j < str.length; j++){
-                if(map.containsKey(str[j])){
-                    if(map.get(str[j]) > 1) continue;
-                    else{
-                        map.put(str[j], map.get(str[j]) + 1);
-                        cnt--;
-                    }
-
+        for(int i =0; i<M; i++){
+            String input =br.readLine();
+            String[] str =input.split(",");
+            for(String s : str){
+                if(map.containsKey(s)) {
+                    map.remove(s);
                 }
             }
-
-            sb.append(cnt).append("\n");
+            sb.append(map.size()).append("\n");
         }
-        System.out.println(sb);
-
-    }
+       System.out.println(sb);
+   }
 }

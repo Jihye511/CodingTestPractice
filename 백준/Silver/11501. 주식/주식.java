ad8397eee39a44be03d99a52851db41ee746b971
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-05 23:52:28 +0900

    [Silver II] Title: 주식, Time: 1080 ms, Memory: 313540 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,31 +1,33 @@
+import javax.swing.*;
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T =Integer.parseInt(br.readLine());
-        for(int t=0; t<T; t++){
-            int N = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int[] day = new int[N];
-            for(int i =0; i<N; i++){
-                day[i] = Integer.parseInt(st.nextToken());
-            }
-            long sum=0L;
-            int num = day[N-1];
-            for(int i =N-2; i>=0; i--){
-               if(num> day[i]){
-                   sum += num-day[i];
-               }else{
-                    num = day[i];
-               }
-            }
-            if(sum<0) sum =0;
-            sb.append(sum).append("\n");
-        }
-        System.out.println(sb);
 
-        }
-    }
+   public static void main(String[] args)throws IOException {
+       StringBuilder sb = new StringBuilder();
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       int T = Integer.parseInt(br.readLine());
+       for(int t =0; t<T; t++){
+           int N = Integer.parseInt(br.readLine());
+           StringTokenizer st = new StringTokenizer(br.readLine());
+           int[] money= new int[N];
+
+           for(int i =0; i<N; i++){
+               money[i] = Integer.parseInt(st.nextToken());
+           }
+           long sum=0;
+           int max = money[N-1];
+           for(int i=N-2; i>=0; i--){
+               if(money[i]<=max){
+                   sum += max - money[i];
+               }else{
+                   max = money[i];
+               }
+           }
+           sb.append(sum).append("\n");
+       }
+       System.out.println(sb);
+   }
+}

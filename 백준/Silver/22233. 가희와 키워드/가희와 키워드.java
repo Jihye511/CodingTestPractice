bb38225916cef0b501cd5815c07edac13fbde531
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-06 20:48:56 +0900

    [Silver III] Title: 가희와 키워드, Time: 1060 ms, Memory: 218660 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -1,4 +1,3 @@
-import javax.imageio.ImageTranscoder;
 import java.io.*;
 import java.util.*;
 
@@ -7,24 +6,23 @@ public class Main {
    public static void main(String[] args)throws IOException {
        StringBuilder sb = new StringBuilder();
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        HashMap<String ,Integer> map = new HashMap<>();
-        for(int i =0; i<N; i++){
-            String str = br.readLine();
-            map.put(str, 1);
-        }
-        for(int i =0; i<M; i++){
-            String input =br.readLine();
-            String[] str =input.split(",");
-            for(String s : str){
-                if(map.containsKey(s)) {
-                    map.remove(s);
-                }
-            }
-            sb.append(map.size()).append("\n");
-        }
+       StringTokenizer st= new StringTokenizer(br.readLine());
+       int N = Integer.parseInt(st.nextToken());
+       int M = Integer.parseInt(st.nextToken());
+       HashSet<String> set = new HashSet<>();
+       for(int i =0; i<N; i++){
+           String word = br.readLine();
+           set.add(word);
+       }
+       for(int i =0; i<M; i++){
+           String[] input = br.readLine().split(",");
+           for(String str : input){
+               if(set.contains(str)){
+                   set.remove(str);
+               }
+           }
+           sb.append(set.size()).append("\n");
+       }
        System.out.println(sb);
    }
 }

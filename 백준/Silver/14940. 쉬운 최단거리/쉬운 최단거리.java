92473661530af353bd0c1b624bf7242aef0175f7
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-08 17:29:33 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 716 ms, Memory: 60580 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,72 +1,67 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Main {
     static int n,m;
     static int[][] map;
-    static int x;
-    static int y;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static boolean[][] visited;
-    static int[][] arr;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    static int sx,sy;
+    static int[][] ans;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
-
+        StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
-        visited = new boolean[n][m];
-        arr = new int[n][m];
-        for(int i =0; i<n; i++){
-            Arrays.fill(arr[i],-1);
-        }
 
+        map = new int[n][m];
+        ans = new int[n][m];
+        boolean[][] v = new boolean[n][m];
         for(int i =0; i<n; i++){
-            st = new StringTokenizer(br.readLine());
+            Arrays.fill(ans[i],-1);
+        }
+        for(int i =0; i<n; i++){
+            st= new StringTokenizer(br.readLine());
             for(int j =0; j<m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 2 ){
-                    x = i;
-                    y = j;
+                if(map[i][j] == 2) {
+                    sx = i;
+                    sy = j;
+                    ans[sx][sy] = 0;
                 }
-                if(map[i][j] ==0) arr[i][j]=0;
+                if(map[i][j]==0) ans[i][j]=0;
             }
         }
-        arr[x][y] =0;
-        visited[x][y] =true;
-        bfs(x,y);
-        for(int[] r : arr){
-            for(int v : r){
-                sb.append(v).append(" ");
+        Queue<int[]> q = new LinkedList<>();
+        map[sx][sy] =0;
+        int[] nx = {-1,1,0,0};
+        int[] ny = {0,0,-1,1};
+        q.offer(new int[]{sx,sy,0});
+        v[sx][sy] = true;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            int amount = cur[2];
+
+            for(int i =0; i<4; i++){
+                int dx = cx + nx[i];
+                int dy = cy + ny[i];
+                if(dx<0 || dx>=n || dy<0 ||dy>=m) continue;
+                if(v[dx][dy]) continue;
+                if(map[dx][dy] == 0) continue;
+                if(ans[dx][dy] != -1) continue;
+                v[dx][dy] = true;
+                ans[dx][dy] = amount+1;
+                q.offer(new int[] {dx,dy,amount+1});
+            }
+        }
+        for(int[] raw : ans){
+            for(int i : raw){
+                sb.append(i).append(" ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
-
-    }
-    public static void bfs(int r, int c){
-        Queue<int[]> q= new LinkedList<>();
-        q.offer(new int[]{r,c});
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-
-            for(int i =0; i<4; i++){
-                int nx = cur[0]+ dx[i];
-                int ny = cur[1] + dy[i];
-                if(nx<0 || nx>= n || ny<0 || ny>=m) continue;
-                if(visited[nx][ny]) continue;
-                if(map[nx][ny] ==0) continue;
-
-                visited[nx][ny] =true;
-                arr[nx][ny] = arr[cur[0]][cur[1]]+1;
-
-                q.offer(new int[] {nx,ny});
-            }
-        }
-
-
     }
 }

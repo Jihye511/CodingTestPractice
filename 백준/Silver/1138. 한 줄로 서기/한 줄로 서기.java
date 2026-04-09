189cf34372cf02f255dc33fadb4505328b903f39
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-10 00:35:21 +0900

    [Silver II] Title: 한 줄로 서기, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,43 +1,34 @@
 import java.io.*;
-import java.text.CollationElementIterator;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] num = new int[N+1];
-        int [] position = new int[N+1];
-        Arrays.fill(position, 0);
-        for(int i = 1; i <= N; i++){
-            num[i] = Integer.parseInt(st.nextToken());
-        }
-        for(int i = 1; i <= N; i++){
-            int cnt = num[i];
-            int idx =1;
-            boolean check = false;
-            while(!check){
-                if(cnt == 0){ // 젤 앞부터 채우기
-                    for(int j = idx; j <= N; j++){
-                        if(position[j] == 0){
-                            position[j] = i;
-                            check = true;
-                            break;
-                        }
-                    }
-                }else if(cnt>0 && position[idx] == 0){
-                    cnt--;
-                    idx++;
-                }else  if(cnt>0 && position[idx] !=0){
-                    idx++;
 
-                }
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for(int i =1; i<=N; i++){
-            sb.append(position[i]).append(" ");
-        }
-        System.out.println(sb);
-    }
+public class Main {
+
+   public static void main(String[] args)throws IOException {
+       StringBuilder sb = new StringBuilder();
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int N = Integer.parseInt(br.readLine());
+       StringTokenizer st = new StringTokenizer(br.readLine());
+       int[] line = new int[N];
+       for(int i =0; i<N; i++){
+           line[i] = Integer.parseInt(st.nextToken());
+       }
+       int[] ans =new int[N];
+       for(int idx = 0; idx<N; idx++){
+           int cur = line[idx];
+           int i =0;
+           while(true){
+               if(cur==0){
+                   if(ans[i]==0) {
+                       ans[i] =idx+1;
+                       break;
+                   }
+               }else if(ans[i]==0) cur--;
+               i++;
+           }
+       }
+       for( int i : ans){
+           sb.append(i).append(" ");
+       }
+       System.out.println(sb);
+   }
 }

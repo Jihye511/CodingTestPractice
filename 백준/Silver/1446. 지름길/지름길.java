1411ad6ffcb72f95d2fd5b132e948a12f1a3c87c
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-13 16:02:48 +0900

    [Silver I] Title: 지름길, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -1,52 +1,56 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Road{
-        int start, end, len;
-        public Road(int start, int end, int len){
+    public static class Road{
+        int start;
+        int end;
+        int shortLoad;
+        public Road(int start, int end, int shortLoad){
             this.start = start;
             this.end = end;
-            this.len = len;
+            this.shortLoad = shortLoad;
         }
     }
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int D = Integer.parseInt(st.nextToken());
-        int[] arr = new int[D+1];
-        Arrays.fill(arr, 10001);
-        List<Road> roads = new ArrayList<>();
-        for(int i =0;i<N; i++){
+        int[] distance = new int[D+1]; // 도착위치까지 온 거리
+        for(int i =0; i<D+1; i++){
+            distance[i] = i;
+        }
+        ArrayList<Road> list = new ArrayList<>();
+        for(int i =0; i<N; i++){
             st= new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            if(b>D || b-a<=c) continue;
-            roads.add(new Road(a,b,c));
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int shortLoad = Integer.parseInt(st.nextToken());
+            list.add(new Road(start, end, shortLoad));
         }
-         Collections.sort(roads, (a,b) -> {
-             if(a.start == b.start) return a.end- b.end;
-             return a.start - b.start;
-         });
+        Collections.sort(list, (a,b)->{
+            if(a.start==b.start && a.end == b.end) return a.shortLoad - b.shortLoad;
+            return a.start - b.start;
+        });
 
-        int idx = 0;
-        int location =0;
-        arr[0] =0;
-        while(location<D){
-            if(idx<roads.size()){
-                Road r = roads.get(idx);
-                if(r.start == location){
-                    arr[r.end]= Math.min(arr[r.end], arr[location] + r.len);
-                    idx++;
-                    continue;
+        for(int i =1;i<=D;i++){
+
+                distance[i] = Math.min(distance[i], distance[i-1]+1);
+
+                // 지금 도착지점까지 저장되어 있는 거리와 해당 지름길을 통한 거리 비교 후 짧은 거 저장
+                for(int j =0; j<N; j++){
+                    if(list.get(j).end == i){
+                        distance[i] = Math.min(distance[i], distance[list.get(j).start]+list.get(j).shortLoad);
+                    }
                 }
-            }
-            arr[location+1] = Math.min(arr[location+1], arr[location]+1);
-            location++;
+
         }
-         System.out.println(arr[D]);
+        System.out.println(distance[D]);
+
     }
 }

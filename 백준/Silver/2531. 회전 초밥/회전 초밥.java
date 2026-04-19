4ddfca22779b989e18eee2883fe4563afbe8ef66
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-19 18:56:11 +0900

    [Silver I] Title: 회전 초밥, Time: 160 ms, Memory: 18764 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -2,44 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        ArrayList<Integer> idx = new ArrayList<>();
-        int[] dish = new int[N];
-        int[] count = new int[d+1];
-        for(int i =0; i<N; i++){
-            int n = Integer.parseInt(br.readLine());
-            dish[i] = n;
-        }
 
+        int[] dish = new int[N];
+        for(int i=0;i<N;i++) dish[i] = Integer.parseInt(br.readLine());
+
+        int[] count = new int[d+1];
         int unique = 0;
-        for(int i =0; i<k; i++){
-            if(count[dish[i]]==0) unique++;
+
+        // 초기 윈도우
+        for(int i=0;i<k;i++){
+            if(count[dish[i]] == 0) unique++;
             count[dish[i]]++;
         }
 
-        int max = unique + (count[c] ==0 ?1:0);
+        int max = unique + (count[c]==0 ? 1 : 0);
 
-        for(int i =1; i<N; i++){
-            //앞
+        // 슬라이딩
+        for(int i=1;i<N;i++){
             int remove = dish[i-1];
             count[remove]--;
             if(count[remove]==0) unique--;
-            //뒤
-            int add =dish[(i+k-1)%N];
-            if(count[add] ==0) unique++;
+
+            int add = dish[(i+k-1)%N];
+            if(count[add]==0) unique++;
             count[add]++;
 
-            int currentMax = unique + (count[c] == 0 ? 1 : 0);
-            max = Math.max(max,currentMax);
+            int total = unique + (count[c]==0 ? 1 : 0);
+            max = Math.max(max, total);
         }
+
         System.out.println(max);
     }
 }

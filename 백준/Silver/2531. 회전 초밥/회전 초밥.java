3c4c1f81a7f5a28a5ee4ab032e3544022ddf6722
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-20 16:33:04 +0900

    [Silver I] Title: 회전 초밥, Time: 232 ms, Memory: 21896 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -10,35 +10,34 @@ public class Main {
         int d = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-
         int[] dish = new int[N];
-        for(int i=0;i<N;i++) dish[i] = Integer.parseInt(br.readLine());
-
-        int[] count = new int[d+1];
-        int unique = 0;
-
-        // 초기 윈도우
-        for(int i=0;i<k;i++){
-            if(count[dish[i]] == 0) unique++;
-            count[dish[i]]++;
+        for(int i =0; i<N; i++){
+            dish[i] = Integer.parseInt(br.readLine());
         }
-
-        int max = unique + (count[c]==0 ? 1 : 0);
-
-        // 슬라이딩
-        for(int i=1;i<N;i++){
-            int remove = dish[i-1];
-            count[remove]--;
-            if(count[remove]==0) unique--;
-
-            int add = dish[(i+k-1)%N];
-            if(count[add]==0) unique++;
-            count[add]++;
-
-            int total = unique + (count[c]==0 ? 1 : 0);
-            max = Math.max(max, total);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //0부터 k-1까지에서 먹은 종류 & 개수
+        for(int i =0; i<k; i++){
+            map.put(dish[i], map.getOrDefault(dish[i], 0)+1);
         }
+        int max = map.size();
+        if(!map.containsKey(c)) max++;
 
+        //슬라이딩 윈도우
+        int end =k;
+        int start = 0;
+        while(true){
+            if(end == k-1) break;
+
+            map.put(dish[end], map.getOrDefault(dish[end],0)+1);
+            map.put(dish[start], map.getOrDefault(dish[start],0)-1);
+            if(map.get(dish[start])==0) map.remove(dish[start]);
+            int temp = map.size();
+            if(!map.containsKey(c)) temp++;
+            max = Math.max(max, temp);
+            end = (end+1)%N;
+            start = (start+1)%N;
+
+        }
         System.out.println(max);
     }
 }

53dab67e1bf9f3e25507f22ffa51e3f4749267d4
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-20 17:33:15 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 524 ms, Memory: 59328 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,34 +1,36 @@
+import javax.imageio.ImageTranscoder;
 import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
-        int[] num = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        st= new StringTokenizer(br.readLine());
+        int num[] = new int[N];
+        for(int i =0; i<N; i++){
             num[i] = Integer.parseInt(st.nextToken());
         }
-
-        Map<Integer, Integer> map = new HashMap<>();
-        int left = 0;
-        int ans = 0;
-
-        for (int right = 0; right < N; right++) {
-            map.put(num[right], map.getOrDefault(num[right], 0) + 1);
-
-            while (map.get(num[right]) > K) {
-                map.put(num[left], map.get(num[left]) - 1);
-                left++;
+        int start =0;
+        int end =0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(num[start], map.getOrDefault(num[start],0)+1);
+        int ans= 0;
+        while(end<N-1){
+            end ++;
+            map.put(num[end], map.getOrDefault(num[end],0)+1);
+            if(map.get(num[end])>K) {
+                //K보다 작거나 같아질때까지 start 이동
+                while(start<end){
+                    map.put(num[start], map.getOrDefault(num[start], 0)-1);
+                    start++;
+                    if(map.get(num[end])==K) break;
+                }
             }
-
-            ans = Math.max(ans, right - left + 1);
+            ans = Math.max(end - start +1, ans);
         }
 
         System.out.println(ans);

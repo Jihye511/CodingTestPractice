39037c2c9785685dda2f54d2c8e5cab0e39ca4e7
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-22 16:38:17 +0900

    [Gold IV] Title: 공유기 설치, Time: 296 ms, Memory: 35072 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,41 +1,45 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
     static int N,C;
-    static int[] home;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N =Integer.parseInt(st.nextToken());
-        C =Integer.parseInt(st.nextToken());
-        home = new int[N];
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        int[] home = new int[N];
         for(int i =0; i<N; i++){
             home[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(home);
-        System.out.println(binary());
+        System.out.println(binarySearch(home));
+
     }
-    public static int binary(){
-        int ans = 0;
-        int lo = 0;
-        int hi = home[N-1] - home[0]+1;
-        while(lo<hi){
-            int mid = (hi-lo)/2 + lo;
-            if(countHome(mid)<C){
-                hi = mid;
-            }else{
+    public static int binarySearch(int[] home){
+        int lo = 1;
+        int hi = home[home.length-1];
+        int ans=0;
+        while(lo<=hi){
+            int mid = (hi-lo)/2 +lo;
+            if(countDistance(home,mid)>=C){
+                ans = mid;
                 lo = mid+1;
+            }else{
+                hi = mid-1;
             }
         }
-        return lo-1;
+        return ans;
     }
-    public static int countHome(int dist){
+    public static int countDistance(int[] home, int distance){
+        boolean[] v = new boolean[N];
+        v[0] = true;
+        int before = home[0];
         int cnt=1;
-        int h = 0;
-        for(int i=1; i<home.length; i++ ){
-            if (home[i] - home[h] >= dist) {
+        for(int i =1; i<N; i++){
+            if(home[i] -before>= distance){
                 cnt++;
-                h = i;
+                before= home[i];
             }
         }
         return cnt;

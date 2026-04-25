aa947bccf7f7dede1b47c784a2fce31da4335217
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-25 18:03:49 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1564 ms, Memory: 143280 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,64 +1,56 @@
 import java.io.*;
-import java.nio.Buffer;
 import java.util.*;
 
 public class Main {
     static int N,M;
-    static int[] sang;
-    static int[] card;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        sang = new int[N];
+        int[] sang = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i =0; i<N; i++){
             sang[i] = Integer.parseInt(st.nextToken());
         }
+        M= Integer.parseInt(br.readLine());
+        int[] cards = new int[M];
         Arrays.sort(sang);
-        M = Integer.parseInt(br.readLine());
-        card = new int[M];
+        int[] ans = new int[M];
         st = new StringTokenizer(br.readLine());
         for(int i =0; i<M; i++){
-            int c = Integer.parseInt(st.nextToken());
-
-            card[i] = upperBound(c) - lowBound(c);
+            cards[i] = Integer.parseInt(st.nextToken());
+            ans[i] = findEnd(sang,cards[i]) - findStart(sang,cards[i]);
         }
-        for(int i : card){
-            sb.append(i).append(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i: ans){
+            sb.append(i+" ");
         }
-
         System.out.println(sb);
 
+
     }
-    public static int lowBound(int key){
-        int lo =0;
-        int hi =N; //둘 다 인덱스 값
-        while(lo<hi){
-            int mid = (hi +lo)/2;
-
-            if(sang[mid]>=key){
-                hi = mid;
+    public static int findStart(int[] sang,int num){
+        int lo= 0;
+        int hi =N-1;
+        while (lo<=hi){
+            int mid = (hi-lo)/2 + lo;
+            if(sang[mid]>=num){
+                hi = mid-1;
             }else{
-                lo = mid+1;
+                lo  =mid+1;
             }
-
         }
         return lo;
     }
-    public static int upperBound(int key){
-        int lo =0;
-        int hi =N; //둘 다 인덱스 값
-        while(lo<hi){
-            int mid = (hi +lo)/2;
-            //key ==sang[mid]일 때, upperbound는 처음으로 초과하는 값을 찾는
-            //거라 lo를 mid+1로 옮김
-            if(sang[mid]>key){
-                hi = mid;
+    public static int findEnd(int[] sang, int num){
+        int lo=0;
+        int hi = N-1;
+        while(lo<=hi){
+            int mid =(hi-lo)/2 + lo;
+            if(sang[mid]<= num){
+                lo= mid+1;
             }else{
-                lo = mid+1;
+                hi =mid-1;
             }
-
         }
         return lo;
     }

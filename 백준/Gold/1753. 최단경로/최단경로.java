44831e6f273f26e8dec54ece2b8824ebddc464ac
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-25 13:22:28 +0900

    [Gold IV] Title: 최단경로, Time: 628 ms, Memory: 106844 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,77 +1,49 @@
-import java.util.*;
 import java.io.*;
-class Node implements Comparable<Node>{
-    int end;
-    int weight;
-    public Node(int end, int weight){
-        this.end= end;
-        this.weight=weight;
-    }
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
-}
-public class Main{
-    static int V,E,K;
-    //map을 2차원배열에 저장할지... 아니면 arrayList로 저장할지... ,고민 (2차원 배열 메모리 터짐)
+import java.util.*;
 
-    static int val =Integer.MAX_VALUE;
-    static boolean[] visited;
-    static List<Node>[] list;
-    static int[] dist;
-    private static final int INF = 100_000_000;
-
-    public static void main(String[] args) throws IOException{
+public class Main {
+    static int V,E;
+    static ArrayList<int[]>[] node;
+    static int start;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        V =Integer.parseInt(st.nextToken());
-        E =Integer.parseInt(st.nextToken());
-        list = new ArrayList[V + 1];
-        dist = new int[V + 1];
-
-        Arrays.fill(dist, INF);
-
-        for(int i = 1; i <= V; i++){
-            list[i] = new ArrayList<>();
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        node= new ArrayList[V+1];
+        for(int i =0; i<V+1; i++){
+            node[i] = new ArrayList<>();
         }
-        visited = new boolean[V+1];
-        K = Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < E; i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            // start에서 end로 가는 weight 가중치
-            list[start].add(new Node(end, weight));
+        start = Integer.parseInt(br.readLine());
+        for(int i =0; i<E; i++){
+            st= new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            node[s].add(new int[]{e,w});
         }
-        StringBuilder sb = new StringBuilder();
-        bfs(K);
-        // 출력 부분
-        for(int i = 1; i <= V; i++){
-            if(dist[i] == INF) sb.append("INF\n");
-            else sb.append(dist[i] + "\n");
-        }
-        System.out.println(sb);
-    }
-    public static void bfs(int start){
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        q.offer(new Node(start,0));
-        dist[start] = 0;
-        while(!q.isEmpty()){
-            Node cur = q.poll();
-            int x = cur.end;
-            int time =cur.weight;
-            if(visited[x] == true) continue;
-            visited[x] = true;
-
-            for(Node node : list[x]){
-                if(dist[node.end] > dist[x] + node.weight){
-                    dist[node.end] = dist[x] + node.weight;
-                    q.add(new Node(node.end, dist[node.end]));
+        int[] value = new int[V+1];
+        Arrays.fill(value,Integer.MAX_VALUE);
+        value[start] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1] -b[1]);
+        pq.offer(new int[]{start, 0});
+        while(!pq.isEmpty()){
+            int[] cur= pq.poll();
+            int idx = cur[0];
+            for(int i =0; i<node[idx].size(); i++){
+                int[] next = node[idx].get(i);
+                if(value[next[0]] > value[idx]+next[1]){
+                    value[next[0]] = value[idx]+next[1];
+                    pq.offer(new int[]{next[0], value[next[0]]});
                 }
             }
         }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i<V+1; i++){
+            if( value[i] == Integer.MAX_VALUE) sb.append("INF").append("\n");
+            else  sb.append(value[i]).append("\n");
+        }
+        System.out.println(sb);
     }
+
 }

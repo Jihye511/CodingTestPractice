da2e84484f60b5f2f897657c79613bc7f3cd791e
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-26 13:31:17 +0900

    [Gold IV] Title: 공유기 설치, Time: 304 ms, Memory: 29620 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -3,43 +3,39 @@ import java.util.*;
 
 public class Main {
     static int N,C;
+    static int[] home;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        int[] home = new int[N];
+        home = new int[N];
         for(int i =0; i<N; i++){
-            home[i] = Integer.parseInt(br.readLine());
+            home[i]= Integer.parseInt(br.readLine());
         }
         Arrays.sort(home);
-        System.out.println(binarySearch(home));
-
-    }
-    public static int binarySearch(int[] home){
-        int lo = 1;
-        int hi = home[home.length-1];
-        int ans=0;
+        int lo=1;
+        int hi=home[N-1];
+        int  ans =0;
         while(lo<=hi){
-            int mid = (hi-lo)/2 +lo;
-            if(countDistance(home,mid)>=C){
+            int mid = (hi-lo)/2+lo;
+            if(countHome(mid)>=C){
                 ans = mid;
                 lo = mid+1;
             }else{
                 hi = mid-1;
             }
         }
-        return ans;
+        System.out.println(ans);
+
     }
-    public static int countDistance(int[] home, int distance){
-        boolean[] v = new boolean[N];
-        v[0] = true;
-        int before = home[0];
+    public static int countHome(int mid){
         int cnt=1;
-        for(int i =1; i<N; i++){
-            if(home[i] -before>= distance){
+        int before = home[0];
+        for(int i =1; i<home.length; i++){
+            if(home[i]-before >=mid){
                 cnt++;
-                before= home[i];
+                before =home[i];
             }
         }
         return cnt;

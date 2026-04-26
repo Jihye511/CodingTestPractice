ff170bc67ce43b41dc1c89d2406d094f30321623
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-26 13:51:33 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,30 +1,27 @@
 import java.io.*;
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer;
         Queue<Integer> q = new LinkedList<>();
-        int days = (100 - progresses[0])/speeds[0];
-        if((100 - progresses[0]) %speeds[0] !=0){
-            days++;
+        ArrayList<Integer>list= new ArrayList<>();
+        for(int i =0; i<progresses.length; i++){
+            int days = (100- progresses[i])/speeds[i];
+            if((100-progresses[i])%speeds[i]!=0) days+=1;
+            
+            q.offer(days);
         }
-        ArrayList<Integer> list= new ArrayList<>();
-        int cnt =1;
-        for(int i =1; i<progresses.length; i++){
-            int tempDay = (100 - progresses[i])/speeds[i];
-            if((100 - progresses[i]) % speeds[i]!=0) tempDay++;
-            if(tempDay <=days){
+        while(!q.isEmpty()){
+            int cur= q.poll();
+            int cnt=1;
+            while(!q.isEmpty() && cur>= q.peek()){
+                q.poll();
                 cnt++;
-            }else{
-                list.add(cnt);
-                cnt =1;
-                days = tempDay;
             }
+            list.add(cnt);
         }
     
-        list.add(cnt);
-        
-        
         answer = new int[list.size()];
         for(int i =0; i<list.size(); i++){
             answer[i] = list.get(i);

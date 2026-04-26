81de0f134a3ece27603046875cdd504c90c835ff
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-26 15:06:52 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,27 +1,28 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    static int n;
-    static int[] p;
-    static int time;
-    public static void main(String[] args) throws IOException{
+public class Main {
+    static int N;
+    static int[] time;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
-        p = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for(int i =0; i<n; i++){
-            p[i] = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        time = new int[N];
+        for(int i =0; i<N; i++){
+            time[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(p);
-        int time=0;
-        int total=0;
-        for(int i=0;i<n;i++){
-            time += p[i];
-            total += time;
+        Arrays.sort(time);
+        int[] wait = new int[N];
+        wait[0] = time[0];
+        for(int i =1; i<N; i++){
+            wait[i] = wait[i-1] + time[i];
         }
-        System.out.println(total);
+        int sum =0;
+        for(int i : wait){
+            sum += i;
+        }
+        System.out.print(sum);
     }
 }

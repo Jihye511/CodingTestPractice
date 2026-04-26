0d4452239395439de3a77fac7e251814fe58e627
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-26 15:15:16 +0900

    [Gold V] Title: 회의실 배정, Time: 504 ms, Memory: 45552 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,28 +1,31 @@
+import javax.swing.*;
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int N;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
-            if(a[1] == b[1]) return a[0]- b[0];
-            else return a[1]-b[1];
-        } );
-        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        ArrayList<int[]> list = new ArrayList<>();
+        for(int i =0; i<N; i++){
+            StringTokenizer st= new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            pq.offer(new int[]{s, e});
+            list.add(new int[]{s, e});
         }
-        int cnt=1;
-        int[] cur = pq.poll();
-        while (!pq.isEmpty()) {
-            int[] next = pq.poll();
-            if(cur[1] <=next[0]){
+        Collections.sort(list, (a,b)->{
+            if(a[1]==b[1]) return a[0]- b[0];
+            else return a[1]-b[1];
+        });
+        int end =-1;
+        int cnt =0;
+        for(int i =0; i<list.size(); i++){
+            int[] cur= list.get(i);
+            if(end<=cur[0]){
+                end = cur[1];
                 cnt++;
-                cur= next;
             }
         }
         System.out.println(cnt);

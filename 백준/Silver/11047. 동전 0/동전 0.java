65ce03bba4ca1a485a24ae2e922c3046d6db5aa6
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-26 15:01:50 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,29 +1,26 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int n,k;
-    static ArrayList<Integer> money;
-    static int result=0;
-
-    public static void main(String[] args)throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
-        n =Integer.parseInt(st.nextToken());
-        k =Integer.parseInt(st.nextToken());
-
-        money = new ArrayList<>();
-        for(int i =0; i<n; i++){
-            int a= Integer.parseInt(br.readLine());
-            money.add(a);
+    static int N,K;
+    static int[] coin;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        coin = new int[N];
+        for(int i =0; i<N; i++){
+            coin[i] = Integer.parseInt(br.readLine());
         }
-        Collections.sort(money,Comparator.reverseOrder());
-        for(int i =0; i<n; i++){
-            if(k>=money.get(i)){
-                result += (k/money.get(i));
-                k %= money.get(i);
+        int cnt=0;
+        for(int i =N-1; i>=0; i--){
+            if(coin[i]<=K){
+                cnt += K/coin[i];
+                K = K%coin[i];
             }
+            if(K==0) break;
         }
-        System.out.println(result);
+        System.out.println(cnt);
     }
-
 }

52d47a43cbe4b9fad234a71667e03930e26d3d2e
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-27 17:59:46 +0900

    [level 3] Title: N으로 표현, Time: 68.89 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,84 +1,50 @@
 import java.io.*;
 import java.util.*;
 class Solution {
+    static HashSet<Integer>[] dp;
     public int solution(int N, int number) {
         int answer = -1;
-        if(N==number) return 1;
-        HashSet<Integer>[] set = new HashSet[9];
-        for(int i=0; i<9; i++){
-            set[i] = new HashSet<>();
-        }
-        boolean check=false;
-        int temp =N;
-        ArrayList<Integer>[] templist = new ArrayList[9];
-        for(int i=0; i<9; i++){
-            templist[i] = new ArrayList<>();
-        }
-        for(int i =1; i<9; i++){            
-            templist[i].add(temp); // 5555...
-            set[i].add(temp);
-            temp = temp *10 +N;
+        dp = new HashSet[9];
+        for(int i =0; i<9; i++){
+            dp[i] = new HashSet<>();
         }
         
-        //5555.. 끼리 계산
-        for(int i =1; i<9; i++){
-            for(int j =1; j<i; j++){
-                int a = templist[j].get(0);
-                int b = templist[i-j].get(0);
-                // System.out.println("a : " + a + " b: "+b);
-                set[i].add(a-b);
-                set[i].add(b-a);
-                set[i].add(a/b);
-                set[i].add(b/a);
-                set[i].add(a+b);
-                set[i].add(a*b);
-                
-            }
-        }
+        dp[1].add(N);
+        if(N == number ) return 1;
         
         for(int i =2; i<9; i++){
-            for(int e =1; e<i; e++){
-                ArrayList<Integer> first = new ArrayList<>(set[e]);//전 꺼
-                ArrayList<Integer> second = new ArrayList<>(set[i-e]);//전 꺼
-                ArrayList<Integer> cur = new ArrayList<>(); 
-                for(int j =0; j<first.size(); j++){
-                    for(int k =0; k<second.size(); k++){
-                        cur.add(first.get(j) + second.get(k));
-                        //빼기,나누기
-
-                        cur.add(first.get(j) - second.get(k));
-                        
-                        if(first.get(j) !=0){
-                            cur.add(second.get(k)/first.get(j));
+            for(int j =1; j<i; j++){
+                //j랑 i-j랑 이렇게 두개 더하기
+                for(int p : dp[j]){
+                    for(int t : dp[i-j]){
+                        //이어붙이기
+                        String s = "";
+                        for(int q =1; q<=i; q++){
+                            s +=N;
                         }
-
-                        
+                        dp[i].add(Integer.parseInt(s));
+                        //더하기
+                        dp[i].add(p+t);
                         //곱하기
-                        cur.add(first.get(j) * second.get(k));        
-                    }            
-                }
-                for(int j=0; j<cur.size(); j++){
-
-                    set[i].add(cur.get(j));
-                }
-                
-                for(int d : set[i]){
-                    if(d == number){
-                        check = true;
-                        break;
+                        dp[i].add(p*t);
+                        //나누기
+                        if(p%t==0 && p/t>0){
+                            dp[i].add(p/t);
+                        }
+                    
+                        //빼기
+                        if(p-t>0){
+                            dp[i].add(p-t);
+                        }
                     }
                 }
-            
-                if(check){
-                    break;
-                }
             }
-            if(check){
-                    answer = i;
-                    break;
-                }
-        
+            if(dp[i].contains(number)){
+                answer = i;
+                break;
+            }
         }
+    
         return answer;
     }
 }

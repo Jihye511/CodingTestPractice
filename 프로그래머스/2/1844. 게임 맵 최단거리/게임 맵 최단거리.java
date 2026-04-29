4e0e99d49d55e82dd39a613111e299a617b67398
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-29 09:57:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.92 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,46 @@
 import java.io.*;
 import java.util.*;
 class Solution {
-    static int[] dx ={-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
     public int solution(int[][] maps) {
         int answer = 0;
-        int m = maps.length;
-        int n = maps[0].length;
-        bfs(maps);
-        if(maps[m-1][n-1] ==1){
-            return -1;
-        }
-        return maps[m-1][n-1];
+        boolean[][]  v= new boolean[maps.length][maps[0].length];
+        answer = bfs(maps, v);
+        if(answer ==1) answer =-1;
+        return answer;
     }
-    public static void bfs(int[][]maps){
-    
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[] {0,0});
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    public static int bfs(int[][] maps, boolean[][]v){
+        int cnt=1;
+        int n = maps.length;
+        int m = maps[0].length;
+        Queue<int[]> q= new LinkedList<>();
+        q.offer(new int[]{0,0,1});
+        v[0][0] = true;
         while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
-            
+            int[] cur= q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int value = cur[2];
             for(int i =0; i<4; i++){
-                int nx = cx+dx[i];
-                int ny = cy +dy[i];
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+                if(v[nx][ny]) continue;
+                if(maps[nx][ny] == 0) continue;
                 
-                if(nx<0|| nx>=maps.length ||ny<0 || ny>=maps[0].length) continue;
+                if(nx == n-1 && ny ==m-1) {
+                    return value+1;
+                    
+                }
                 
-                if(maps[nx][ny] !=1) continue;
+                v[nx][ny] = true;
+                q.offer(new int[]{nx,ny,value+1});
                 
-                maps[nx][ny] +=maps[cx][cy];
-                q.offer(new int[]{nx,ny});
                 
             }
+           
         }
+         return cnt;
     }
 }

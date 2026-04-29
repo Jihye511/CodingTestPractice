5838cb2fec44fca7da49a6f04d7fddec26caa836
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-29 14:06:52 +0900

    [level 3] Title: 입국심사, Time: 91.82 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,31 +1,27 @@
 import java.io.*;
 import java.util.*;
-
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
         Arrays.sort(times);
-        answer = binary(n,times);
-        return answer;
-    }
-    //lowerbound
-    public static long binary(int n, int[] times){
-        long lo = 1;
-        long hi = (long) n* (long)times[times.length-1];
-        while(lo<hi){
+        
+        long lo = 0; 
+        long hi = (long)times[times.length-1] * n;
+        while(lo<=hi){
             long mid = (hi-lo)/2 + lo;
-            if(count(mid, times) < n){
-                lo = mid+1;
+            if(count(times, mid)>=n){
+                answer = mid;
+                hi = mid-1;
             }else{
-                hi = mid;
+                lo = mid+1;
             }
         }
-        return lo;
+        return answer;
     }
-    public static long count(long time, int[] times){
-        long cnt =0;
-        for(int i =0; i<times.length; i++){
-            cnt += time/times[i];
+    public static long count(int[] times, long mid){
+        long cnt=0;
+        for(int t: times){
+            cnt += (long)mid/t;
         }
         return cnt;
     }

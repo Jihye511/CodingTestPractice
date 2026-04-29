de66bb99c013e8a5d883c8226bbf7761189741a2
Jihye511 <129932517+Jihye511@users.noreply.github.com>
2026-04-29 14:49:00 +0900

    [level 3] Title: 가장 먼 노드, Time: 25.98 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -4,41 +4,45 @@ class Solution {
     static ArrayList<Integer>[] node;
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        node = new ArrayList[n+1];
-        for(int i =0;i<n+1; i++){
+        node= new ArrayList[n+1];
+        for(int i=0; i<=n; i++){
             node[i] = new ArrayList<>();
         }
-        for(int i =0; i<edge.length; i++){
-            int a = edge[i][0];
-            int b = edge[i][1];
-            node[a].add(b);
-            node[b].add(a);
+        for(int i =0; i< edge.length; i++){
+            int s = edge[i][0];
+            int e = edge[i][1];
+            node[s].add(e);
+            node[e].add(s);
         }
-        answer = bfs(n);
-        return answer;
-    }
-    public static int bfs(int n){
-        int max = 0;
-        Queue<int[]> q = new LinkedList<>();
-        boolean[] v = new boolean[n+1];
+        //bfs
+        Queue<int[]> q= new LinkedList<>();
+        boolean[] v= new boolean[n+1];
         q.offer(new int[]{1,0});
-        v[1] = true;
+        v[1] =true;
+        int[] depth = new int[n+1];
+        depth[1] = 0;
+        int max =0;
         while(!q.isEmpty()){
-            
-            int size = q.size();
-            max =size;
-            for(int i=0; i<size; i++){
-                int[] cur = q.poll();
-                for(int j =0; j<node[cur[0]].size(); j++){
-                    int next = node[cur[0]].get(j);
-                    if(!v[next]){
-                        v[next]  =true;
-                        q.offer(new int[]{next, cur[1]+1});
-                    }
+            int[] cur = q.poll();
+            int x = cur[0];
+            for(int i =0; i<node[x].size(); i++){
+                int next = node[x].get(i);
+                if(!v[next]){
+                    v[next]= true;
+                    depth[next] = cur[1]+1;
+                    q.offer(new int[]{next, cur[1]+1});
+                    max = Math.max(max, depth[next]);
                 }
             }
-
         }
-        return max;
+        
+        int count = 0;
+        for(int i = 1; i <= n; i++){
+            if(depth[i] == max){
+                count++;
+            }
+        }
+        return count;
     }
+    
 }
